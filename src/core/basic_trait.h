5feb13b0b3c4655e5d267e910dacc6e3f3f2d803
zhuling1 <249002031@qq.com>
2022-04-26 21:23:49 +0800

    update trait type

--- src/core/basic_trait.h
+++ src/core/basic_trait.h
@@ -79,6 +79,63 @@ struct Vector;
 template<size_t N>
 struct Matrix;
 
+namespace detail {
 
+template<typename T, size_t N = 0u>
+struct is_vector_impl : std::false_type {};
+
+template<typename T, size_t N>
+struct is_vector_impl<Vector<T, N>, N> : std::true_type {};
+
+template<typename T, size_t N>
+struct is_vector_impl<Vector<T, N>, 0u> : std::true_type {};
+
+template<typename T, size_t N = 0u>
+struct is_matrix_impl : std::false_type {};
+
+template<size_t N>
+struct is_matrix_impl<Matrix<N>, N> : std::true_type {};
+
+template<size_t N>
+struct is_matrix_impl<Matrix<N>, 0u> : std::true_type {};
+
+template<typename T>
+struct vector_element_impl {
+    using type = T;
+};
+
+template<typename T>
+struct vector_dimension_impl {
+    static constexpr auto value = static_cast<size_t>(1u);
+};
+
+template<typename T, size_t N>
+struct vector_dimension_impl<Vector<T, N>> {
+    static constexpr auto value = N;
+};
+
+template<typename T>
+struct matrix_dimension_impl {
+    static constexpr auto value = static_cast<size_t>(1u);
+};
+
+template<size_t N>
+struct matrix_dimension_impl<Matrix<N>> {
+    static constexpr auto value = N;
+};
+
+template<typename U, typename V>
+struct is_vector_same_dimension_impl : std::false_type {};
+
+template<typename U, typename V, size_t N>
+struct is_vector_same_dimension_impl<Vector<U, N>, Vector<V, N>> : std::true_type {};
+
+template<typename... T>
+struct is_vector_all_same_dimension_impl : std::true_type {};
+
+template<typename First, typename... Other>
+struct is_vector_all_same_dimension_impl<First, Other...> : std::conjunction<is_vector_same_dimension_impl<First, Other>...> {};
+
+}// namespace detail
 
 }// namespace sycamore
--- src/core/basic_type.h
+++ src/core/basic_type.h
@@ -21,19 +21,52 @@ SCM_NODISCARD constexpr auto operator""_gb(unsigned long long bytes) noexcept {
     return static_cast<size_t>(bytes * 1024u * 1024u * 1024u);
 }
 
-//template<typename T, size_t N>
-//struct VectorStorage {
-//    static_assert(false, "Invalid vector storage");
-//};
+}// namespace size_literals
 
+namespace detail {
+template<typename T, size_t N>
+struct VectorStorage {
+    static_assert(always_false_v<T>, "Invalid vector storage");
+};
 
-//template<typename T>
-//struct alignas(sizeof(T) * 2): VectorStorage<T, 2> {
-//
-//}
+template<typename T>
+struct alignas(sizeof(T) * 2) VectorStorage<T, 2> {
+    T x, y;
+    explicit constexpr VectorStorage(T s = {}) noexcept : x{s}, y{s} {}
+    constexpr VectorStorage(T x, T y) noexcept : x{x}, y{y} {}
+};
 
-}
+template<typename T>
+struct alignas(sizeof(T) * 4) VectorStorage<T, 3> {
+    T x, y, z;
+    explicit constexpr VectorStorage(T s = {}) noexcept : x{s}, y{s}, z{s} {}
+    constexpr VectorStorage(T x, T y, T z) noexcept : x{x}, y{y}, z{z} {}
+};
 
+template<typename T>
+struct alignas(sizeof(T) * 4) VectorStorage<T, 4> {
+    T x, y, z, w;
+    explicit constexpr VectorStorage(T s = {}) noexcept : x{s}, y{s}, z{s}, w{s} {}
+    constexpr VectorStorage(T x, T y, T z, T w) noexcept : x{x}, y{y}, z{z}, w{w} {}
+};
 
+}// namespace detail
 
-}// namespace sycamore::core
+template<typename T, size_t N>
+struct Vector : public detail::VectorStorage<T, N> {
+    static constexpr auto dimension = N;
+    using value_type = T;
+    using Storage = detail::VectorStorage<T, N>;
+    static_assert(std::disjunction_v<
+                      std::is_same<T, bool>,
+                      std::is_same<T, float>,
+                      std::is_same<T, int>,
+                      std::is_same<T, uint>>,
+                  "Invalid vector type");
+    static_assert(N == 2 || N == 3 || N == 4, "Invalid vector dimension");
+    using Storage::VectorStorage;
+    [[nodiscard]] constexpr T &operator[](size_t index) noexcept { return (&(this->x))[index]; }
+    [[nodiscard]] constexpr const T &operator[](size_t index) const noexcept { return (&(this->x))[index]; }
+};
+
+}// namespace sycamore
