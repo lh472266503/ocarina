9feca446ec4c23cf4847838782aa9989e7a24a61
zhuling1 <249002031@qq.com>
2022-05-02 21:37:55 +0800

    Create stats_line_num.py

---
+++ src/stats_line_num.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+
+import os
+
+count = 0
+
+com = 0
+
+inCom = False
+
+num_file = 0
+
+for root,dirs,files in os.walk(os.path.join(os.getcwd(), "src")):
+    for file in files:
+        fn = os.path.join(root,file)
+        if "ext\\" in fn:
+            continue
+        if "gui" in fn:
+            continue
+        if "tests" in fn:
+            continue
+        if "stats.py" in fn:
+            continue
+        if "jitify" in fn:
+            continue
+        if "sdk_pt" in fn:
+            continue
+        if ".natvis" in fn:
+            continue
+        try:
+            print(file)
+            
+            f = open(fn, "r")
+            count += len(f.readlines())
+        except :
+            print(fn)
+
+        
+        
+        num_file += 1
+
+
+print(count, num_file)
+        
+class C:
+    pass
+class A:
+    pass
+class B(A, C):
+    pass
+    
+print(issubclass(B,C))  
