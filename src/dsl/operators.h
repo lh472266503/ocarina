dd9213ae9235f74b40b1e75e2ea99ed8d7a4cb97
zhuling1 <249002031@qq.com>
2022-05-16 16:03:29 +0800

    Update operators.h

--- src/dsl/operators.h
+++ src/dsl/operators.h
@@ -10,14 +10,20 @@
 
 namespace katana {
 
-template<typename T>
-requires katana::is_dsl_v<T>
-    KTN_NODISCARD inline auto operator-(T &&expr) noexcept {
-    using Ret = std::remove_cvref_t<decltype(-std::declval<katana::expr_value_t<T>>())>;
-    return katana::def<Ret>(katana::FunctionBuilder::current()->unary(
-        Type::of<Ret>(),
-        UnaryOp::NEGATIVE,
-        katana::detail::extract_expression(std::forward<T>(expr))));
-}
-
 }// namespace katana
+
+#define KTN_MAKE_DSL_UNARY_OPERATOR(op, tag)                                                   \
+    template<typename T>                                                                       \
+    requires katana::is_dsl_v<T>                                                               \
+        KTN_NODISCARD inline auto operator op(T &&expr) noexcept {                             \
+        using Ret = std::remove_cvref_t<decltype(op std::declval<katana::expr_value_t<T>>())>; \
+        return katana::def<Ret>(katana::FunctionBuilder::current()->unary(                     \
+            katana::Type::of<Ret>(),                                                           \
+            katana::UnaryOp::tag,                                                              \
+            katana::detail::extract_expression(std::forward<T>(expr))));                       \
+    }
+
+KTN_MAKE_DSL_UNARY_OPERATOR(+, POSITIVE)
+KTN_MAKE_DSL_UNARY_OPERATOR(-, NEGATIVE)
+KTN_MAKE_DSL_UNARY_OPERATOR(!, NOT)
+KTN_MAKE_DSL_UNARY_OPERATOR(~, BIT_NOT)
