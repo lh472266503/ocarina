8978805528a40f4e67d1007d5d1c5d1327926552
zhuling1 <249002031@qq.com>
2022-06-02 17:02:34 +0800

    Update logging.h

--- src/core/logging.h
+++ src/core/logging.h
@@ -78,16 +78,20 @@ inline void error_if_not(bool predicate, Args &&...args) {
 }
 }// namespace katana::core
 
-#define KTN_SOURCE_LOCATION __FILE__ , ":", __LINE__
-
-#define KTN_DEBUG(...) \
-    ::katana::core::debug(__VA_ARGS__);
+#define KTN_SOURCE_LOCATION __FILE__, ":", __LINE__
 
 #define SET_LOG_LEVEL(lv) \
     ::katana::core::set_log_level(spdlog::level::level_enum::lv);
 
+#define KTN_DEBUG(...) \
+    ::katana::core::debug(__VA_ARGS__);
+#define KTN_DEBUG_FORMAT(FMT, ...) \
+    KTN_DEBUG(katana::format(FMT, __VA_ARGS__));
+
 #define KTN_INFO(...) \
     ::katana::core::info(__VA_ARGS__);
+#define KTN_INFO_FORMAT(FMT, ...) \
+    KTN_INFO(katana::format(FMT, __VA_ARGS__));
 
 #define KTN_WARNING(...) \
     ::katana::core::warning(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
@@ -95,6 +99,8 @@ inline void error_if_not(bool predicate, Args &&...args) {
     ::katana::core::warning_if(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
 #define KTN_WARNING_IF_NOT(...) \
     ::katana::core::warning_if_not(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
+#define KTN_WARNING_FORMAT(FMT, ...) \
+    KTN_WARNING(katana::format(FMT, __VA_ARGS__));
 
 #define KTN_EXCEPTION(...) \
     ::katana::core::exception(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
@@ -102,6 +108,8 @@ inline void error_if_not(bool predicate, Args &&...args) {
     ::katana::core::exception_if(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
 #define KTN_EXCEPTION_IF_NOT(...) \
     ::katana::core::exception_if_not(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
+#define KTN_EXCEPTION_FORMAT(FMT, ...) \
+    KTN_EXCEPTION(katana::format(FMT, __VA_ARGS__));
 
 #define KTN_ERROR(...) \
     ::katana::core::error(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
@@ -109,3 +117,5 @@ inline void error_if_not(bool predicate, Args &&...args) {
     ::katana::core::error_if(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
 #define KTN_ERROR_IF_NOT(...) \
     ::katana::core::error_if_not(__VA_ARGS__, "\n    Source: ", KTN_SOURCE_LOCATION);
+#define KTN_ERROR_FORMAT(FMT, ...) \
+    KTN_ERROR(katana::format(FMT, __VA_ARGS__));
