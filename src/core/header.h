340d68de4bc68b451f533ec6a20a15ad7e406b07
zhuling1 <249002031@qq.com>
2022-06-08 18:13:24 +0800

    Update header.h

--- src/core/header.h
+++ src/core/header.h
@@ -53,6 +53,28 @@
 #define OC_OFFSET_OF(type, member) offsetof(type, member)
 #endif
 
+#ifdef __cplusplus
+#define OC_EXTERN_C extern "C"
+#define OC_NOEXCEPT noexcept
+#else
+#define OC_EXTERN_C
+#define OC_NOEXCEPT
+#endif
+
+#ifdef _MSC_VER
+#define OC_FORCE_INLINE inline
+#define OC_NEVER_INLINE __declspec(noinline)
+#define OC_DLL
+#define OC_EXPORT_API OC_EXTERN_C __declspec(dllexport)
+#define OC_IMPORT_API OC_EXTERN_C __declspec(dllimport)
+#else
+#define OC_FORCE_INLINE [[gnu::always_inline, gnu::hot]] inline
+#define OC_NEVER_INLINE [[gnu::noinline]]
+#define OC_DLL
+#define OC_EXPORT_API OC_EXTERN_C [[gnu::visibility("default")]]
+#define OC_IMPORT_API OC_EXTERN_C
+#endif
+
 #define OC_USING_SV using namespace std::string_view_literals;
 
 #define OC_ASSERT(...) assert(__VA_ARGS__)
