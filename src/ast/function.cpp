76a29ab58ba60c4e1b11bcabbe0132996e64d07c
zhuling1 <249002031@qq.com>
2022-06-22 13:28:24 +0800

    删除多余文件，整理function

--- src/ast/function.cpp
+++ src/ast/function.cpp
@@ -18,84 +18,77 @@ ocarina::vector<Function *> &Function::_function_stack() noexcept {
 }
 
 void Function::return_(ConstExprPtr expression) noexcept {
-    _impl->return_(expression);
+    if (expression) {
+        _ret = expression->type();
+    }
+    create_statement<ReturnStmt>(expression);
 }
 
 Function::Function(Function::Tag tag) noexcept
-    : _impl(ocarina::make_unique<Impl>(tag)) {
+    : _tag(tag) {
+    push_scope();
 }
 
 const ScopeStmt *Function::body() const noexcept {
-    return _impl->_scope_stack.front();
+    return _scope_stack.front();
 }
 
 ScopeStmt *Function::body() noexcept {
-    return _impl->_scope_stack.front();
+    return _scope_stack.front();
 }
 
 ConstExprPtr Function::argument(const Type *type) noexcept {
-    return _impl->argument(type);
+    Variable variable(type, Variable::Tag::LOCAL, next_variable_uid());
+    _arguments.push_back(variable);
+    return _ref(variable);
 }
 
 ConstExprPtr Function::reference_argument(const Type *type) noexcept {
-    return _impl->reference_argument(type);
+    Variable variable(type, Variable::Tag::REFERENCE, next_variable_uid());
+    _arguments.push_back(variable);
+    return _ref(variable);
 }
 
 ConstExprPtr Function::local(const Type *type) noexcept {
-    auto ret = _impl->create_expression<RefExpr>(Variable(type, Variable::Tag::LOCAL,
-                                                          _impl->next_variable_uid()));
+    auto ret = create_expression<RefExpr>(Variable(type, Variable::Tag::LOCAL,
+                                                          next_variable_uid()));
     body()->add_var(ret->variable());
     return ret;
 }
 
 ConstExprPtr Function::literal(const Type *type, LiteralExpr::value_type value) noexcept {
-    return _impl->create_expression<LiteralExpr>(type, value);
+    return create_expression<LiteralExpr>(type, value);
 }
 
 ConstExprPtr Function::binary(const Type *type, BinaryOp op, ConstExprPtr lhs, ConstExprPtr rhs) noexcept {
-    return _impl->create_expression<BinaryExpr>(type, op, lhs, rhs);
+    return create_expression<BinaryExpr>(type, op, lhs, rhs);
 }
 
 ConstExprPtr Function::unary(const Type *type, UnaryOp op, ConstExprPtr expression) noexcept {
-    return _impl->create_expression<UnaryExpr>(type, op, expression);
+    return create_expression<UnaryExpr>(type, op, expression);
 }
 
 IfStmt *Function::if_(ConstExprPtr expr) noexcept {
-    return _impl->create_statement<IfStmt>(expr);
-}
-
-void Function::mark_variable_usage(uint uid, Usage usage) noexcept {
-    _impl->mark_variable_usage(uid, usage);
+    return create_statement<IfStmt>(expr);
 }
 
 ocarina::span<const Variable> Function::arguments() const noexcept {
-    return _impl->arguments();
-}
-
-const ScopeStmt *Function::current_scope() const noexcept {
-    return _impl->current_scope();
+    return _arguments;
 }
 
 const Type *Function::return_type() const noexcept {
-    return _impl->return_type();
+    return _ret;
 }
 
-Function::Tag Function::tag() const noexcept {
-    return _impl->tag();
-}
-
-bool Function::is_callable() const noexcept {
-    return _impl->is_callable();
-}
-
-bool Function::is_kernel() const noexcept {
-    return _impl->is_kernel();
-}
 void Function::assign(ConstExprPtr lhs, ConstExprPtr rhs) noexcept {
-    _impl->assign(lhs, rhs);
+    create_statement<AssignStmt>(lhs, rhs);
 }
 uint64_t Function::hash() const noexcept {
-    return _impl->hash();
+    if (!_hash_computed) {
+        _hash = _compute_hash();
+        _hash_computed = true;
+    }
+    return _hash;
 }
 void Function::postprocess() noexcept {
     
--- src/ast/function.h
+++ src/ast/function.h
@@ -10,6 +10,7 @@
 #include "type.h"
 #include "variable.h"
 #include "expression.h"
+#include "statement.h"
 
 namespace ocarina {
 
@@ -17,7 +18,7 @@ class Statement;
 class ScopeStmt;
 class IfStmt;
 
-class OC_AST_API Function {
+class OC_AST_API Function: public concepts::Noncopyable {
 public:
     enum struct Tag : uint {
         KERNEL,
@@ -25,8 +26,15 @@ public:
     };
 
 private:
-    class Impl;
-    ocarina::unique_ptr<Impl> _impl{};
+    const Type *_ret{nullptr};
+    ocarina::vector<ocarina::unique_ptr<Expression>> _all_expressions;
+    ocarina::vector<ocarina::unique_ptr<Statement>> _all_statements;
+    ocarina::vector<Variable> _arguments;
+    ocarina::vector<Usage> _variable_usages;
+    ocarina::vector<ScopeStmt *> _scope_stack;
+    mutable uint64_t _hash{0};
+    mutable bool _hash_computed{false};
+    Tag _tag{Tag::CALLABLE};
 
 private:
     static ocarina::vector<Function *> &_function_stack() noexcept;
@@ -46,7 +54,19 @@ private:
         ret.postprocess();
         return ret;
     }
-
+    template<typename Expr, typename... Args>
+    [[nodiscard]] auto create_expression(Args &&...args) {
+        auto expr = ocarina::make_unique<Expr>(std::forward<Args>(args)...);
+        auto ret = expr.get();
+        _all_expressions.push_back(std::move(expr));
+        return ret;
+    }
+    [[nodiscard]] ConstExprPtr _ref(Variable variable) noexcept {
+        return create_expression<RefExpr>(variable);
+    }
+    [[nodiscard]] uint64_t _compute_hash() const noexcept {
+        return 6545689;
+    }
 public:
     [[nodiscard]] static Function *current() noexcept {
         return _function_stack().back();
@@ -55,6 +75,37 @@ public:
     static auto define_callable(Func &&func) noexcept {
         return _define(Tag::CALLABLE, std::forward<Func>(func));
     }
+    [[nodiscard]] const ScopeStmt *current_scope() const noexcept {
+        return _scope_stack.back();
+    }
+    [[nodiscard]] ScopeStmt *current_scope() noexcept {
+        return _scope_stack.back();
+    }
+
+    template<typename Stmt, typename... Args>
+    auto create_statement(Args &&...args) {
+        auto stmt = ocarina::make_unique<Stmt>(std::forward<Args>(args)...);
+        auto ret = stmt.get();
+        _all_statements.push_back(std::move(stmt));
+        current_scope()->add_stmt(ret);
+        return ret;
+    }
+    void push_scope() {
+        auto scope = ocarina::make_unique<ScopeStmt>();
+        _scope_stack.push_back(scope.get());
+        _all_statements.push_back(std::move(scope));
+    }
+    [[nodiscard]] uint next_variable_uid() noexcept {
+        auto ret = _variable_usages.size();
+        _variable_usages.push_back(Usage::NONE);
+        return ret;
+    }
+    void pop_scope() {
+        _scope_stack.pop_back();
+    }
+    void mark_variable_usage(uint uid, Usage usage) noexcept {
+        _variable_usages[uid] = usage;
+    }
     template<typename Func>
     static auto define_kernel(Func &&func) noexcept {
         return _define(Tag::KERNEL, std::forward<Func>(func));
@@ -62,7 +113,6 @@ public:
     Function() noexcept = default;
     explicit Function(Tag tag) noexcept;
     void assign(ConstExprPtr lhs, ConstExprPtr rhs) noexcept;
-    void mark_variable_usage(uint uid, Usage usage) noexcept;
     void return_(ConstExprPtr expression) noexcept;
     [[nodiscard]] ConstExprPtr argument(const Type *type) noexcept;
     [[nodiscard]] ConstExprPtr reference_argument(const Type *type) noexcept;
@@ -75,18 +125,12 @@ public:
     [[nodiscard]] ScopeStmt *body() noexcept;
     [[nodiscard]] uint64_t hash() const noexcept;
     [[nodiscard]] ocarina::span<const Variable> arguments() const noexcept;
-    [[nodiscard]] const ScopeStmt *current_scope() const noexcept;
-    [[nodiscard]] Tag tag() const noexcept;
-    [[nodiscard]] bool is_callable() const noexcept;
-    [[nodiscard]] bool is_kernel() const noexcept;
+    [[nodiscard]] Tag tag() const noexcept { return _tag; }
+    [[nodiscard]] bool is_callable() const noexcept { return _tag == Tag::CALLABLE; }
+    [[nodiscard]] bool is_kernel() const noexcept { return _tag == Tag::KERNEL; }
     [[nodiscard]] const Type *return_type() const noexcept;
     void postprocess() noexcept;
 };
 
 }// namespace ocarina
 
-#ifndef NDEBUG
-
-#include "function_impl.h"
-
-#endif
