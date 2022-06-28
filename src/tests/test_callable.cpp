8e3f8f7ed804bdfd48d249bc6b82fb426d5825db
Zero <249002031@qq.com>
2022-06-28 19:20:36 +0800

    Update test_callable.cpp

--- src/tests/test_callable.cpp
+++ src/tests/test_callable.cpp
@@ -22,94 +22,11 @@ using std::endl;
 using namespace ocarina;
 
 template<typename T>
-auto func(T a, T b) {
-    //    T ret = (a + b) * b;
+T func(T a, T b) {
 
-    //    for_range(a,[&](auto x) {
-    //    $comment(---)
-    //    });
-//    a = 1.5f;
-//    return a + b * b + 1.f;
-//    ::ocarina::range(a, 9 + b, 3 + a + b) / [&](auto v) noexcept {
-//        v += 1;
-//    };
-//    Var<std::array<int, 5>> arr;
-//    a = (a + a * b);
-//    a = arr[0];
-//    return a + 1.5f;
-//    $for(v, b, 9) {
-//        a += v;
-//    };
-//    return a * 5.1f;
-    //    Var cond(1);
-    //    auto fun = [&]() {
-    //        a += 1;
-    //        return a < 15;
-    //    };
-    //    while_(a < 5, [&] {
-    //        a += 1;
-    //        $comment(sddsfdfsa)
-    //    });
-    //
-    //    $while(fun()){
-    //        $comment(sddsfd-- -- -fsa)};
-    //
-    //    $switch(a) {
-    //        $case(1) {
-    //            $comment(daf)
-    //                $break;
-    //        };
-    //        $case(2){
-    //            $comment(9089)};
-    //        $default{
-    //            $comment(default_)};
-    //    };
-    //
-//    switch_(a, [&] {
-//        case_(2, [&] {
-//            $comment(adsfdsf)
-//        });
-//        default_([&] {
-//            $comment(90890887879)
-//        });
-//    });
-////
-//    switch_(a)
-//        .case_(2, [&] {
-//            $comment(adsfdsf)
-//            break_();
-//        })
-//        .case_(3, [&]{
-//            $comment(456747567)
-//        })
-//        .default_([&]{
-//            $comment(-------)
-//        });
-    //
-        $if(1) {
-            $comment(adsfadsf)
-        }
-        $elif([&](){
-            a = b + 1;
-            return a > 0;
-        }()) {
-            $comment(adsfadsf)
-        }
-//        $else {
-//            a = 2;
-//        }
-        ;
-    //
-    //    if_(cond, [&] {
-    //        comment("this is comment");
-    //        a = 1;
-    //    }).elif_(cond, [&] {
-    //          a = b;
-    //      }).else_([&] {
-    //        a = b;
-    //    });
-
-//    return a;
+    T f = 1.9f + a;
+    T c = a + b * a + 1.5f;
+    return c + c;
 }
 
 int main(int argc, char *argv[]) {
