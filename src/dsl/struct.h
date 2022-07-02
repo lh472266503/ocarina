7c78e1b1541afd7a503a7c7e9b838ebc85a69379
zhuling1 <249002031@qq.com>
2022-07-02 19:04:35 +0800

    update struct

--- src/dsl/struct.h
+++ src/dsl/struct.h
@@ -5,8 +5,58 @@
 #pragma once
 
 #include "ast/type_registry.h"
+#include "dsl/var.h"
 
+#define OC_MAKE_STRUCT_MEMBER(member) \
+    Var<std::remove_cvref_t<decltype(this_type::member)>> member{};
 
-#define OC_STRUCT(S, ...) \
+#define OC_MAKE_COMPUTABLE_BODY(S, ...)                                        \
+    namespace detail {                                                         \
+    template<>                                                                 \
+    struct Computable<S> {                                                     \
+        using this_type = S;                                                   \
+        MAP(OC_MAKE_STRUCT_MEMBER, ##__VA_ARGS__)                              \
+    private:                                                                   \
+        const Expression *_expression{nullptr};                                \
+                                                                               \
+    public:                                                                    \
+        [[nodiscard]] auto expression() const noexcept { return _expression; } \
+                                                                               \
+    protected:                                                                 \
+        explicit Computable(const Expression *e) noexcept : _expression{e} {}  \
+        Computable(Computable &&) noexcept = default;                          \
+        Computable(const Computable &) noexcept = default;                     \
+    };                                                                         \
+    }
+#define OC_MAKE_VAR_BODY(S, ...)                                                                \
+    template<>                                                                                  \
+    struct Var<S> : public detail::Computable<S> {                                              \
+        MAP(OC_MAKE_STRUCT_MEMBER, ##__VA_ARGS__)                                               \
+        explicit Var(const Expression *expression) noexcept                                     \
+            : detail::Computable<S>(expression) {}                                              \
+                                                                                                \
+        Var() noexcept : Var(Function::current()->local(Type::of<S>())) {}                      \
+                                                                                                \
+        template<typename Arg>                                                                  \
+        requires concepts::non_pointer<std::remove_cvref_t<Arg>> &&                             \
+            concepts::assign_able<expr_value_t<std::remove_cvref_t<S>>, expr_value_t<Arg>>      \
+            Var(Arg &&arg) : Var() {                                                            \
+            assign(*this, std::forward<Arg>(arg));                                              \
+        }                                                                                       \
+        explicit Var(detail::ArgumentCreation) noexcept                                         \
+            : Var(Function::current()->argument(Type::of<S>())) {                               \
+        }                                                                                       \
+        explicit Var(detail::ReferenceArgumentCreation) noexcept                                \
+            : Var(Function::current()->reference_argument(Type::of<S>())) {}                    \
+        template<typename Arg>                                                                  \
+        requires concepts::assign_able<expr_value_t<std::remove_cvref_t<S>>, expr_value_t<Arg>> \
+        void operator=(Arg &&arg) {                                                             \
+            assign(*this, std::forward<Arg>(arg));                                              \
+        }                                                                                       \
+    };
+
+#define OC_STRUCT(S, ...)                       \
     OC_MAKE_STRUCT_REFLECTION(S, ##__VA_ARGS__) \
-    OC_MAKE_STRUCT_DESC(S, ##__VA_ARGS__)
+    OC_MAKE_STRUCT_DESC(S, ##__VA_ARGS__)       \
+    OC_MAKE_COMPUTABLE_BODY(S, ##__VA_ARGS__) \
+    OC_MAKE_VAR_BODY(S, ##__VA_ARGS__) \
--- src/rt/hit.h
+++ src/rt/hit.h
@@ -14,6 +14,18 @@ struct alignas(16) Hit {
     float2 bary;
 };
 
+
+//OC_MAKE_COMPUTABLE_BODY(ocarina::Hit, inst_id, prim_id, bary)
+
+
 OC_STRUCT(ocarina::Hit, inst_id, prim_id, bary)
+
+//template<>
+//struct Var<ocarina::Hit> : public detail::Computable<ocarina::Hit>{
+//    using this_type = ocarina::Hit;
+////    Var<std::remove_cvref_t<decltype(this_type::inst_id)> inst_id{};
+////    Var < std::remove_cvref_t<decltype(this_type::prim_id)> prim_id{};
+////    Var < std::remove_cvref_t<decltype(this_type::bary)> bary{};
+//};
 
 }// namespace ocarina
