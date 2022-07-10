843ef43bf4368725d810daf551861d64bf08706a
zhuling1 <249002031@qq.com>
2022-07-10 22:35:32 +0800

    Update pool.h

--- src/core/pool.h
+++ src/core/pool.h
@@ -12,19 +12,50 @@ namespace ocarina {
 template<typename T>
 class Pool : public concepts::Noncopyable {
 public:
-    static constexpr size_t element_count = 64;
+    static constexpr size_t element_count = 32;
+    static constexpr bool trivially = std::is_trivially_constructible_v<T>;
 
 private:
     ocarina::vector<T *> _blocks;
-    ocarina::vector<T *> _available_ptr;
-public:
-    template<typename ...Args>
-    [[nodiscard]] auto create(Args &&...args) noexcept {
+    ocarina::vector<T *> _available_objects;
 
+    void _enlarge() {
+        T *ptr = ocarina::allocate<T>(element_count);
+        _blocks.push_back(ptr);
+        _available_objects.reserve(element_count);
+        for (int i = 0; i < element_count; ++i) {
+            _available_objects.push_back(&(ptr[i]));
+        }
+    }
+
+public:
+    ~Pool() {
+        if constexpr (!trivially) {
+            for (auto &ptr : _available_objects) {
+                ptr->~T();
+            }
+        }
+        for (auto &ptr : _blocks) {
+            ocarina::delete_with_allocator(ptr);
+        }
+    }
+
+    template<typename... Args>
+    [[nodiscard]] auto create(Args &&...args) noexcept {
+        if (_available_objects.empty()) {
+            _enlarge();
+        }
+        T *ptr = _available_objects.back();
+        _available_objects.pop_back();
+        std::construct_at(ptr, OC_FORWARD(args)...);
+        return ptr;
     }
 
     void recycle(T *ptr) noexcept {
-
+        if (!trivially) {
+            ptr->~T();
+        }
+        _available_objects.push_back(ptr);
     }
 };
 
