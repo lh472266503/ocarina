973fa9e91dca31cc48bf05e0eb8f23b85fd64964
Zero <249002031@qq.com>
2022-07-10 13:31:11 +0800

    Update pool.h

--- src/core/pool.h
+++ src/core/pool.h
@@ -5,15 +5,27 @@
 #pragma once
 
 #include "core/stl.h"
+#include "core/concepts.h"
 
 namespace ocarina {
 
 template<typename T>
-class Pool {
+class Pool : public concepts::Noncopyable {
 public:
     static constexpr size_t element_count = 64;
-private:
 
+private:
+    ocarina::vector<T *> _blocks;
+    ocarina::vector<T *> _available_ptr;
+public:
+    template<typename ...Args>
+    [[nodiscard]] auto create(Args &&...args) noexcept {
+
+    }
+
+    void recycle(T *ptr) noexcept {
+
+    }
 };
 
 }// namespace ocarina
