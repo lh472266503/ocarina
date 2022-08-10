1a5915da466beb18317990a3c174943fbbf86a80
zhuling <249002031@qq.com>
2022-08-10 13:55:16 +0800

    Update buffer.h

--- src/rhi/resources/buffer.h
+++ src/rhi/resources/buffer.h
@@ -76,7 +76,7 @@ public:
     template<typename Index>
     requires ocarina::is_integral_v<expr_value_t<Index>>
     OC_NODISCARD auto
-    read(Index &&index) {
+    read(Index &&index) const {
         const UniformBinding &uniform = Function::current()->get_uniform_var(Type::of<Buffer<T>>(),
                                                                              handle(), Variable::Tag::BUFFER);
         return make_expr<Buffer<T>>(uniform.expression()).read(OC_FORWARD(index));
