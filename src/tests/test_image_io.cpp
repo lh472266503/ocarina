0584ea7d951719868c1f8b6a243bdaea9489d394
zhuling <249002031@qq.com>
2022-08-12 14:15:24 +0800

    Update test_image_io.cpp

--- src/tests/test_image_io.cpp
+++ src/tests/test_image_io.cpp
@@ -3,13 +3,27 @@
 //
 
 #include "util/image.h"
+#include "core/stl.h"
+#include "dsl/common.h"
+#include "rhi/context.h"
+#include "rhi/common.h"
+#include <windows.h>
+#include "math/base.h"
+#include "math/geometry.h"
+#include "util/image.h"
+#include "dsl/common.h"
 
 using namespace ocarina;
 
-int main() {
-    auto path = R"(E:/work/compile/ocarina/res/test.png)";
+int main(int argc, char *argv[]) {
+    fs::path path(argv[0]);
+    Context context(path.parent_path());
+    context.clear_cache();
+    Device device = context.create_device("cuda");
+    Stream stream = device.create_stream();
+    auto path1 = R"(E:/work/compile/ocarina/res/test.png)";
     auto path2 = R"(E:/work/compile/ocarina/res/test.tga)";
-    auto image = Image::load(path, LINEAR);
+    auto image = Image::load(path1, LINEAR);
     image.save(path2);
     return 0;
 }
