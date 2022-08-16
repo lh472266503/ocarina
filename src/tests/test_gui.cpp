928291f2eda616ded17ccb47906921d299d557f5
zhuling <249002031@qq.com>
2022-08-16 19:19:01 +0800

    update gui

---
+++ src/tests/test_gui.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Zero on 2022/8/16.
+//
+
+
+#include "core/stl.h"
+#include "dsl/common.h"
+#include "rhi/context.h"
+#include "rhi/common.h"
+#include <windows.h>
+#include "math/base.h"
+#include "math/geometry.h"
+#include "util/image_io.h"
+#include "dsl/common.h"
+#include "gui/window.h"
+
+using namespace ocarina;
+
+int main(int argc, char *argv[]) {
+    Window window{"Display", make_uint2(300)};
+    window.run([&]{
+
+    });
+}
