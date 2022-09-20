0c66b45fbe1427f5af40eb3e9899a8ca9fd4e119
zhuling1 <249002031@qq.com>
2022-09-20 22:22:52 +0800

    Update operators.h

--- src/dsl/operators.h
+++ src/dsl/operators.h
@@ -30,58 +30,74 @@ OC_MAKE_DSL_UNARY_OPERATOR(~, BIT_NOT)
 
 #undef OC_MAKE_DSL_UNARY_OPERATOR
 
-#define OC_MAKE_DSL_BINARY_OPERATOR(op, tag)                                            \
-    template<typename Lhs, typename Rhs>                                                \
-    requires ocarina::any_dsl_v<Lhs, Rhs> &&                                            \
-             ocarina::is_basic_v<ocarina::expr_value_t<Lhs>> &&                         \
-             ocarina::is_basic_v<ocarina::expr_value_t<Rhs>> [                          \
-             [nodiscard]] inline auto                                                   \
-    operator op(Lhs &&lhs, Rhs &&rhs) noexcept {                                        \
-        using namespace std::string_view_literals;                                      \
-        static constexpr bool is_logic_op = #op == "||"sv || #op == "&&"sv;             \
-        static constexpr bool is_bit_op = #op == "|"sv || #op == "&"sv || #op == "^"sv; \
-        static constexpr bool is_bool_lhs = ocarina::is_boolean_expr_v<Lhs>;            \
-        static constexpr bool is_bool_rhs = ocarina::is_boolean_expr_v<Rhs>;            \
-        using NormalRet = std::remove_cvref_t<                                          \
-            decltype(std::declval<ocarina::expr_value_t<Lhs>>() op                      \
-                         std::declval<ocarina::expr_value_t<Rhs>>())>;                  \
-        using Ret = std::conditional_t<is_bool_lhs && is_logic_op, bool, NormalRet>;    \
-        return ocarina::make_expr<Ret>(ocarina::Function::current()->binary(            \
-            ocarina::Type::of<Ret>(),                                                   \
-            ocarina::BinaryOp::tag,                                                     \
-            ocarina::detail::extract_expression(std::forward<Lhs>(lhs)),                \
-            ocarina::detail::extract_expression(std::forward<Rhs>(rhs))));              \
-    }
+#define OC_MAKE_DSL_BINARY_OPERATOR(op, tag, trait)                                                                           \
+    template<typename Lhs, typename Rhs>                                                                                      \
+    requires ocarina::any_dsl_v<Lhs, Rhs> &&                                                                                  \
+        ocarina::is_basic_v<ocarina::expr_value_t<Lhs>> &&                                                                    \
+        ocarina::is_basic_v<ocarina::expr_value_t<Rhs>>                                                                       \
+    [[nodiscard]] inline auto                                                                                                 \
+    operator op(Lhs &&lhs, Rhs &&rhs) noexcept {                                                                              \
+        using namespace std::string_view_literals;                                                                            \
+        static constexpr bool is_logic_op = #op == "||"sv || #op == "&&"sv;                                                   \
+        static constexpr bool is_bit_op = #op == "|"sv || #op == "&"sv || #op == "^"sv;                                       \
+        static constexpr bool is_bool_lhs = ocarina::is_boolean_expr_v<Lhs>;                                                  \
+        static constexpr bool is_bool_rhs = ocarina::is_boolean_expr_v<Rhs>;                                                  \
+        using NormalRet = std::remove_cvref_t<                                                                                \
+            decltype(std::declval<ocarina::expr_value_t<Lhs>>() op                                                            \
+                         std::declval<ocarina::expr_value_t<Rhs>>())>;                                                        \
+        using Ret = std::conditional_t<is_bool_lhs && is_logic_op, bool, NormalRet>;                                          \
+        return ocarina::make_expr<Ret>(ocarina::Function::current()->binary(                                                  \
+            ocarina::Type::of<Ret>(),                                                                                         \
+            ocarina::BinaryOp::tag,                                                                                           \
+            ocarina::detail::extract_expression(std::forward<Lhs>(lhs)),                                                      \
+            ocarina::detail::extract_expression(std::forward<Rhs>(rhs))));                                                    \
+    }                                                                                                                         \
+    namespace ocarina {                                                                                                       \
+    namespace detail {                                                                                                        \
+    template<typename Lhs, typename Rhs>                                                                                      \
+    requires none_dsl_v<Lhs, Rhs>                                                                                             \
+    decltype(std::declval<Lhs>() op std::declval<Rhs>()) trait##_func(Lhs lhs, Rhs rhs);                                      \
+    template<typename Lhs, typename Rhs>                                                                                      \
+    requires any_dsl_v<Lhs, Rhs>                                                                                              \
+        Var<decltype(std::declval<expr_value_t<Lhs>>() op std::declval<expr_value_t<Rhs>>())> trait##_func(Lhs lhs, Rhs rhs); \
+    template<typename Lhs, typename Rhs>                                                                                      \
+    struct trait {                                                                                                            \
+        using type = decltype(detail::trait##_func(std::declval<Lhs>(), std::declval<Rhs>()));                                \
+    };                                                                                                                        \
+    }                                                                                                                         \
+    template<typename... T>                                                                                                   \
+    using trait##_t = typename detail::trait<T...>::type;                                                                     \
+    };// namespace ocarina
 
-OC_MAKE_DSL_BINARY_OPERATOR(+, ADD)
-OC_MAKE_DSL_BINARY_OPERATOR(-, SUB)
-OC_MAKE_DSL_BINARY_OPERATOR(*, MUL)
-OC_MAKE_DSL_BINARY_OPERATOR(/, DIV)
-OC_MAKE_DSL_BINARY_OPERATOR(%, MOD)
-OC_MAKE_DSL_BINARY_OPERATOR(&, BIT_AND)
-OC_MAKE_DSL_BINARY_OPERATOR(|, BIT_OR)
-OC_MAKE_DSL_BINARY_OPERATOR(^, BIT_XOR)
-OC_MAKE_DSL_BINARY_OPERATOR(<<, SHL)
-OC_MAKE_DSL_BINARY_OPERATOR(>>, SHR)
-OC_MAKE_DSL_BINARY_OPERATOR(&&, AND)
-OC_MAKE_DSL_BINARY_OPERATOR(||, OR)
-OC_MAKE_DSL_BINARY_OPERATOR(==, EQUAL)
-OC_MAKE_DSL_BINARY_OPERATOR(!=, NOT_EQUAL)
-OC_MAKE_DSL_BINARY_OPERATOR(<, LESS)
-OC_MAKE_DSL_BINARY_OPERATOR(<=, LESS_EQUAL)
-OC_MAKE_DSL_BINARY_OPERATOR(>, GREATER)
-OC_MAKE_DSL_BINARY_OPERATOR(>=, GREATER_EQUAL)
+OC_MAKE_DSL_BINARY_OPERATOR(+, ADD, add)
+OC_MAKE_DSL_BINARY_OPERATOR(-, SUB, sub)
+OC_MAKE_DSL_BINARY_OPERATOR(*, MUL, mul)
+OC_MAKE_DSL_BINARY_OPERATOR(/, DIV, div)
+OC_MAKE_DSL_BINARY_OPERATOR(%, MOD, mod)
+OC_MAKE_DSL_BINARY_OPERATOR(&, BIT_AND, bit_and)
+OC_MAKE_DSL_BINARY_OPERATOR(|, BIT_OR, bit_or)
+OC_MAKE_DSL_BINARY_OPERATOR(^, BIT_XOR, bit_xor)
+OC_MAKE_DSL_BINARY_OPERATOR(<<, SHL, shl)
+OC_MAKE_DSL_BINARY_OPERATOR(>>, SHR, shr)
+OC_MAKE_DSL_BINARY_OPERATOR(&&, AND, and_op)
+OC_MAKE_DSL_BINARY_OPERATOR(||, OR, or_op)
+OC_MAKE_DSL_BINARY_OPERATOR(==, EQUAL, equal)
+OC_MAKE_DSL_BINARY_OPERATOR(!=, NOT_EQUAL, not_equal)
+OC_MAKE_DSL_BINARY_OPERATOR(<, LESS, less)
+OC_MAKE_DSL_BINARY_OPERATOR(<=, LESS_EQUAL, less_eq)
+OC_MAKE_DSL_BINARY_OPERATOR(>, GREATER, greater)
+OC_MAKE_DSL_BINARY_OPERATOR(>=, GREATER_EQUAL, greater_equal)
 
 #undef OC_MAKE_DSL_BINARY_OPERATOR
 
-#define OC_MAKE_DSL_ASSIGN_OP(op)                                                         \
-    template<typename Lhs, typename Rhs>                                                  \
-    requires requires {                                                                   \
-                 std::declval<Lhs &>() op## = std::declval<ocarina::expr_value_t<Rhs>>(); \
-             }                                                                            \
-    void operator op##=(const ocarina::Var<Lhs> &lhs, Rhs &&rhs) {                        \
-        auto x = lhs op std::forward<Rhs>(rhs);                                           \
-        ocarina::Function::current()->assign(lhs.expression(), x.expression());           \
+#define OC_MAKE_DSL_ASSIGN_OP(op)                                                \
+    template<typename Lhs, typename Rhs>                                         \
+    requires requires {                                                          \
+        std::declval<Lhs &>() op## = std::declval<ocarina::expr_value_t<Rhs>>(); \
+    }                                                                            \
+    void operator op##=(const ocarina::Var<Lhs> &lhs, Rhs &&rhs) {               \
+        auto x = lhs op std::forward<Rhs>(rhs);                                  \
+        ocarina::Function::current()->assign(lhs.expression(), x.expression());  \
     }
 
 OC_MAKE_DSL_ASSIGN_OP(+)
