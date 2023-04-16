df27575cc7335ce13d601e7091276ebac3d2ce60
Zero <249002031@qq.com>
2023-04-16 20:51:35 +0800

    Update test_image_io.cpp

--- src/tests/test_image_io.cpp
+++ src/tests/test_image_io.cpp
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
 //    context.clear_cache();
     Device device = context.create_device("cuda");
     Stream stream = device.create_stream();
-    auto path1 = R"(E:/work/compile/ocarina/res/test.png)";
-    auto path2 = R"(E:/work/compile/ocarina/res/test.jpg)";
+    auto path1 = R"(E:/work/test_dir/D5.png)";
+    auto path2 = R"(E:/work/test_dir/D5.jpg)";
     auto image_io = ImageIO::load(path1, LINEAR);
 
     auto image = device.create_texture(image_io.resolution(), image_io.pixel_storage());
@@ -38,16 +38,17 @@ int main(int argc, char *argv[]) {
         Var<uint> min_y = max(0u, dispatch_idx().y - r);
         Var<uint> max_y = min(res.y - 1, dispatch_idx().y + r);
 
-        Var<float4> var = make_float4(0.f);
+
+        Var<float4> var = make_float4(dispatch_idx().x / cast<float>(res.x));
         Var<uint> count = 0u;
-        $for(x, min_x, max_x) {
-            $for(y, min_y, max_y) {
-                Var v = img.read<float4>(x, y);
-                var += v;
-                count += 1;
-            };
-        };
-        var /= count.cast<float>();
+//        $for(x, min_x, max_x) {
+//            $for(y, min_y, max_y) {
+//                Var v = img.read<float4>(x, y);
+//                var += v;
+//                count += 1;
+//            };
+//        };
+//        var /= count.cast<float>();
         image_out.write(dispatch_idx().xy(), var);
     };
 
