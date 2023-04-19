ec14e6d612512aa383798ea5ab1d0f9ddbd022cd
Zero <249002031@qq.com>
2023-04-19 23:01:34 +0800

    Update test_trait.cpp

--- src/tests/test_trait.cpp
+++ src/tests/test_trait.cpp
@@ -71,35 +71,41 @@ template<typename Ret, typename T>
 
 }// namespace ocarina::detail
 
-#define OC_MEMBER(type, name)           \
+#define OC_SERIALIZE_MEMBER(type, name) \
     type name{};                        \
     optional<Var<type>> _device_##name; \
     uchar _offset_##name;
 
+#define OC_ENCODE_ELEMENT(name)             \
+    _offset_##name = datas.size() - offset; \
+    detail::encode(datas, name);
+
+#define OC_DECODE_ELEMENT(name) _device_##name = detail::decode<decltype(name)>(values, _offset_##name);
+
+#define OC_ENCODE_DECODE(...)                                            \
+    uint _data_size{0u};                                                 \
+                                                                         \
+public:                                                                  \
+    void encode(vector<float> &datas) noexcept {                         \
+        uint offset = datas.size();                                      \
+        MAP(OC_ENCODE_ELEMENT, __VA_ARGS__)                              \
+        _data_size = datas.size() - offset;                              \
+    }                                                                    \
+    void decode(const DataAccessor<float> *da) noexcept {                \
+        Array<float> values = da->read_dynamic_array<float>(_data_size); \
+        MAP(OC_DECODE_ELEMENT, __VA_ARGS__)                              \
+    }
+
 struct Test {
-    OC_MEMBER(float, a);
-    OC_MEMBER(float, b);
+    OC_SERIALIZE_MEMBER(float, a);
+    OC_SERIALIZE_MEMBER(float, b);
 
-    uint data_size{0u};
-    void encode(vector<float> &datas) noexcept {
-        _offset_a = datas.size();
-        datas.push_back(a);
-        _offset_b = datas.size();
-        datas.push_back(b);
-    }
-
-    void decode(const DataAccessor<float> *da) noexcept {
-        Array<float> values = da->read_dynamic_array<float>(2);
-        _device_a = detail::decode<decltype(a)>(values, _offset_a);
-        _device_b = detail::decode<decltype(b)>(values, _offset_b);
-    }
+    OC_ENCODE_DECODE(a, b)
 };
 
 int main() {
 
-        Test t;
-
-
+    Test t;
 
     //    cout << is_vector_v<float2>;
     //    cout << (!is_dsl_v<Float3>) && is_vector_v<Float3> ;
