e73f665a92bec22a3372ca4247976be5e1525aae
Zero <249002031@qq.com>
2023-04-19 19:12:53 +0800

    Update test_raygenkernel.cpp

--- src/tests/test_raygenkernel.cpp
+++ src/tests/test_raygenkernel.cpp
@@ -75,8 +75,8 @@ int main(int argc, char *argv[]) {
     Device device = context.create_device("cuda");
     Stream stream = device.create_stream();
     Printer::instance().init(device);
-    auto path1 = R"(E:/work/compile/ocarina/res/test.png)";
-    auto path2 = R"(E:/work/compile/ocarina/res/test.jpg)";
+    auto path1 = R"(E:/work/test_dir/D5.png)";
+    auto path2 = R"(E:/work/test_dir/D5.jpg)";
     auto image_io = ImageIO::load(path1, LINEAR);
     auto image = device.create_texture(image_io.resolution(), image_io.pixel_storage());
     stream << image.upload_sync(image_io.pixel_ptr());
