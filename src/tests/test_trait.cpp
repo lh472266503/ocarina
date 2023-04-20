08813eb3622fe4627981e876fc7f65a257fe1027
Zero <249002031@qq.com>
2023-04-21 00:11:14 +0800

    Update test_trait.cpp

--- src/tests/test_trait.cpp
+++ src/tests/test_trait.cpp
@@ -11,16 +11,53 @@ using namespace ocarina;
 namespace ocarina {
 namespace detail {
 
-template<typename T>
-requires is_std_vector_v<T> || is_basic_v<T>
+template<typename value_ty>
+requires (is_std_vector_v<value_ty> && is_scalar_v<typename value_ty::value_type>) || is_basic_v<value_ty>
 struct SharedData {
-    using element_ty = T;
-    T _host_value;
-    dsl_t<T> _device_value;
-    uint offset;
+public:
+    value_ty _host_value{};
+    optional<dsl_t<value_ty>> _device_value{};
+    uint offset{};
 
-    [[nodiscard]] T hv() const noexcept { return _host_value; }
-    [[nodiscard]] dsl_t<T> dv() const noexcept { return _device_value; }
+public:
+    SharedData(value_ty val) : _host_value(val) {}
+
+    [[nodiscard]] value_ty &hv() const noexcept { return _host_value; }
+    [[nodiscard]] dsl_t<value_ty> &dv() const noexcept { return *_device_value; }
+
+    template<typename T>
+    void encode(vector<T> &data) noexcept {
+        if constexpr (is_scalar_v<value_ty>) {
+            data.push_back(bit_cast<T>(_host_value));
+        } else if constexpr (is_vector_v<value_ty>) {
+            for (int i = 0; i < vector_dimension_v<value_ty>; ++i) {
+                data.push_back(bit_cast<T>(_host_value[i]));
+            }
+        } else if constexpr (is_matrix_v<value_ty>) {
+            for (int i = 0; i < matrix_dimension_v<value_ty>; ++i) {
+                for (int j = 0; j < matrix_dimension_v<value_ty>; ++j) {
+                    data.push_back(bit_cast<T>(_host_value[i][j]));
+                }
+            }
+        } else {
+            static_assert(always_false_v<value_ty>);
+        }
+    }
+
+    [[nodiscard]] uint size() noexcept {
+        if constexpr (is_scalar_v<value_ty>) {
+            static_assert(sizeof(value_ty) <= sizeof(float));
+            return 1;
+        } else if constexpr (is_vector_v<value_ty>) {
+            return vector_dimension_v<value_ty>;
+        } else if constexpr (is_matrix_v<value_ty>) {
+            return sqr(matrix_dimension_v<value_ty>);
+        } else if constexpr (is_std_vector_v<value_ty>) {
+            return _host_value.size();
+        } else {
+            static_assert(always_false_v<value_ty>);
+        }
+    }
 };
 
 }
@@ -60,9 +97,10 @@ struct Test {
 
 int main() {
 
-    Test t;
+    //    Test t;
 
-    Float a;
+    //    Float a{nullptr};
+    //    Float a{};
 
     //    cout << is_vector_v<float2>;
     //    cout << (!is_dsl_v<Float3>) && is_vector_v<Float3> ;
