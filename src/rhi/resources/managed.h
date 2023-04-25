795c1e009727f8dff64f1a3a46370c6582a3c19a
Zero <249002031@qq.com>
2023-04-25 23:57:58 +0800

    Update managed.h

--- src/rhi/resources/managed.h
+++ src/rhi/resources/managed.h
@@ -96,13 +96,6 @@ public:
 template<typename T>
 class PolymorphicElement;
 
-template<typename T>
-class ISerializable;
-
-template<typename T>
-requires(sizeof(T) == sizeof(float))
-struct DataAccessor;
-
 template<typename T, typename U = float>
 class ManagedWrapper : public Managed<T>,
                        public PolymorphicElement<U>,
@@ -111,50 +104,52 @@ public:
     using Super = Managed<T>;
 
 private:
-    uint _id{InvalidUI32};
+    Serialize<uint> _index{InvalidUI32};
+    Serialize<uint> _size{InvalidUI32};
     ResourceArray *_resource_array{};
 
 public:
     ManagedWrapper() = default;
+    OC_SERIALIZABLE_FUNC(U, _index,_size)
     void init(ResourceArray &resource_array) noexcept { _resource_array = &resource_array; }
 
     [[nodiscard]] uint datas_size() const noexcept override {
-        return sizeof(_id) + sizeof(uint);
+        return sizeof(_index) + sizeof(uint);
     }
     void fill_datas(ManagedWrapper<U> &datas) const noexcept override {
-        datas.push_back(bit_cast<U>(_id));
+        datas.push_back(bit_cast<U>(_index.hv()));
         datas.push_back(bit_cast<U>(uint(Super::host_ty::size())));
     }
 
     explicit ManagedWrapper(ResourceArray &resource_array) : _resource_array(&resource_array) {}
     void register_self() noexcept {
-        _id = _resource_array->emplace(Super::device());
+        _index = _resource_array->emplace(Super::device());
     }
 
-    [[nodiscard]] uint index() const noexcept { return _id; }
+    [[nodiscard]] uint index() const noexcept { return _index.hv(); }
 
     template<typename Index>
     requires concepts::all_integral<expr_value_t<Index>>
     OC_NODISCARD auto read(Index &&index) const noexcept {
-        return _resource_array->buffer<T>(_id).read(OC_FORWARD(index));
+        return _resource_array->buffer<T>(_index.hv()).read(OC_FORWARD(index));
     }
 
     template<typename Target, typename Offset>
     requires is_integral_expr_v<Offset>
     OC_NODISCARD auto byte_read(Offset &&offset) const noexcept {
-        return _resource_array->byte_buffer(_id).read<Target>(OC_FORWARD(offset));
+        return _resource_array->byte_buffer(_index.hv()).read<Target>(OC_FORWARD(offset));
     }
 
     template<typename Elm, typename Offset>
     requires is_integral_expr_v<Offset>
     [[nodiscard]] Array<Elm> read_dynamic_array(uint size, Offset &&offset) const noexcept {
-        return _resource_array->byte_buffer(_id).read_dynamic_array<Elm>(size, OC_FORWARD(offset));
+        return _resource_array->byte_buffer(_index.hv()).read_dynamic_array<Elm>(size, OC_FORWARD(offset));
     }
 
     template<typename Index, typename Val>
     requires concepts::integral<expr_value_t<Index>> && concepts::is_same_v<T, expr_value_t<Val>>
     void write(Index &&index, Val &&elm) {
-        _resource_array->buffer<T>(_id).write(OC_FORWARD(index), OC_FORWARD(elm));
+        _resource_array->buffer<T>(_index.hv()).write(OC_FORWARD(index), OC_FORWARD(elm));
     }
 };
 
