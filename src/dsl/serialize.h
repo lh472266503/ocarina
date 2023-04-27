24f1be0819a863d9bf10deba9dc135358006a638
Zero <249002031@qq.com>
2023-04-27 22:42:48 +0800

    Update serialize.h

--- src/dsl/serialize.h
+++ src/dsl/serialize.h
@@ -90,6 +90,13 @@ public:
         OC_ASSERT(has_device_value());
         return *_device_value;
     }
+    [[nodiscard]] dsl_t<value_ty> auto_value() const noexcept {
+        if (has_device_value()) {
+            return dv();
+        } else {
+            return dsl_t<value_ty>(hv());
+        }
+    }
     void encode(ManagedWrapper<T> &data) const noexcept override {
         if constexpr (is_scalar_v<value_ty>) {
             data.push_back(bit_cast<T>(hv()));
