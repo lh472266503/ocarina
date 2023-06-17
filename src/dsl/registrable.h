bf270934e590544c0acd9b23d46a59f2aa7cf03c
Zero <249002031@qq.com>
2023-06-17 20:03:48 +0800

    Update registrable.h

--- src/dsl/registrable.h
+++ src/dsl/registrable.h
@@ -38,8 +38,8 @@ public:
     using Super = Buffer<T>;
 
 public:
-    explicit RegistrableBuffer(ResourceArray *resource_array)
-        : Registrable(resource_array) {}
+    explicit RegistrableBuffer(ResourceArray &resource_array)
+        : Registrable(&resource_array) {}
 
     RegistrableBuffer() = default;
     void register_self() noexcept {
