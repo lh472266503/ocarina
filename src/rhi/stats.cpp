9126b497434246813d79b0b20473856a8cf66306
Zero <249002031@qq.com>
2023-08-24 20:13:13 +0800

    update stats

--- src/rhi/stats.cpp
+++ src/rhi/stats.cpp
@@ -24,19 +24,32 @@ void MemoryStats::destroy_instance() {
 }
 
 void MemoryStats::on_buffer_allocate(ocarina::handle_ty handle, size_t size, std::string name) {
-
+    with_lock([&] {
+        _buffer_size += size;
+        _buffer_map.insert(make_pair(handle, BufferData{name, size}));
+    });
 }
 
 void MemoryStats::on_buffer_free(ocarina::handle_ty handle) {
-
+    with_lock([&] {
+        _buffer_size -= buffer_size(handle);
+        _buffer_map.erase(handle);
+    });
 }
 
-void MemoryStats::on_tex_allocate(ocarina::handle_ty handle, size_t size, std::string name) {
-
+void MemoryStats::on_tex_allocate(ocarina::handle_ty handle, uint2 res, std::string name) {
+    with_lock([&] {
+        _tex_size += res.x * res.y;
+        _tex_map.insert(make_pair(handle, TexData{name, res}));
+    });
 }
 
 void MemoryStats::on_tex_free(ocarina::handle_ty handle) {
-
+    with_lock([&] {
+        uint2 res = tex_res(handle);
+        _tex_size -= res.x * res.y;
+        _tex_map.erase(handle);
+    });
 }
 
 }// namespace ocarina
--- src/rhi/stats.h
+++ src/rhi/stats.h
@@ -5,10 +5,21 @@
 #pragma once
 
 #include "core/stl.h"
+#include "core/basic_types.h"
+#include "core/thread_safety.h"
 
 namespace ocarina {
 
-class MemoryStats {
+class MemoryStats : public thread_safety<> {
+    struct BufferData {
+        string name;
+        size_t size;
+    };
+    struct TexData {
+        string name;
+        uint2 res;
+    };
+
 private:
     static MemoryStats *s_stats;
     MemoryStats() = default;
@@ -18,17 +29,28 @@ private:
     MemoryStats operator=(MemoryStats &&) = delete;
 
 private:
-    size_t _buffer_size;
-    size_t _tex_size;
-    map<handle_ty, pair<string, size_t>> _buffer_map;
-    map<handle_ty, pair<string, size_t>> _tex_map;
+    size_t _buffer_size{};
+    size_t _tex_size{};
+    map<handle_ty, BufferData> _buffer_map;
+    map<handle_ty, TexData> _tex_map;
+
+private:
+    [[nodiscard]] size_t buffer_size(handle_ty handle) const noexcept {
+        OC_ASSERT(_buffer_map.contains(handle));
+        return _buffer_map.at(handle).size;
+    }
+
+    [[nodiscard]] uint2 tex_res(handle_ty handle) const noexcept {
+        OC_ASSERT(_tex_map.contains(handle));
+        return _tex_map.at(handle).res;
+    }
 
 public:
     static MemoryStats &instance();
     static void destroy_instance();
     void on_buffer_allocate(handle_ty handle, size_t size, string name = "");
     void on_buffer_free(handle_ty handle);
-    void on_tex_allocate(handle_ty handle, size_t size, string name = "");
+    void on_tex_allocate(handle_ty handle, uint2 res, string name = "");
     void on_tex_free(handle_ty handle);
 };
 
