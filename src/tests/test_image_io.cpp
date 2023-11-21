aa7e9569e760a540bdd6379a1555712cc9c84d0a
GraphZou <906520989@qq.com>
2023-11-21 14:29:10 +0800

    Update test_image_io.cpp

--- src/tests/test_image_io.cpp
+++ src/tests/test_image_io.cpp
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
 //    context.clear_cache();
     Device device = context.create_device("cuda");
     Stream stream = device.create_stream();
-    auto path1 = R"(E:/work/test_dir/D5.png)";
-    auto path2 = R"(E:/work/test_dir/D5.jpg)";
+    auto path1 = R"(D:\work\renderer\Vision\gallery\cbox-sss.png)";
+    auto path2 = R"(D:\work\renderer\Vision\gallery\cbox-sss.jpg)";
     auto image_io = ImageIO::load(path1, LINEAR);
 
     auto image = device.create_texture(image_io.resolution(), image_io.pixel_storage());
@@ -41,23 +41,27 @@ int main(int argc, char *argv[]) {
 
         Var<float4> var = make_float4(dispatch_idx().x / cast<float>(res.x));
         Var<uint> count = 0u;
-//        $for(x, min_x, max_x) {
-//            $for(y, min_y, max_y) {
-//                Var v = img.read<float4>(x, y);
-//                var += v;
-//                count += 1;
-//            };
-//        };
-//        var /= count.cast<float>();
+        $for(x, min_x, max_x) {
+            $for(y, min_y, max_y) {
+                Var v = img.read<float4>(x, y);
+                var += v;
+                count += 1;
+            };
+        };
+        var /= count.cast<float>();
         image_out.write(dispatch_idx().xy(), var);
     };
-
+    kernel.function()->set_raytracing(true);
+//    kernel.function()->set_raytracing(false);
     auto shader = device.compile(kernel);
+    Clock clk;
     stream << shader(image).dispatch(image_io.resolution());
     stream << synchronize()
            << image_out.download_sync(image_io.pixel_ptr())
            << commit();
     image_io.save(path2);
+
+    cout << "elapse ms " << clk.elapse_ms() << endl;
 
     return 0;
 }
