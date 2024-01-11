af0775c14bc089eb98df0f206ebc9200c666542e
GraphZou <906520989@qq.com>
2024-01-11 18:38:24 +0800

    update function corrector

--- src/ast/function_corrector.cpp
+++ src/ast/function_corrector.cpp
@@ -6,18 +6,40 @@
 
 namespace ocarina {
 
-void FunctionCorrector::visit(const ScopeStmt *scope) {
-    for (const Statement *stmt : scope->statements()) {
-        stmt->accept(*this);
-    }
-}
-
 void FunctionCorrector::traverse(Function &function) noexcept {
     visit(function.body());
 }
 
 void FunctionCorrector::apply() noexcept {
     traverse(*_function);
+}
+
+void FunctionCorrector::visit(const ScopeStmt *scope) {
+    for (const Statement *stmt : scope->statements()) {
+        stmt->accept(*this);
+    }
+}
+
+void FunctionCorrector::visit(const AssignStmt *stmt) {
+    stmt->lhs()->accept(*this);
+}
+void FunctionCorrector::visit(const BinaryExpr *stmt) {
+}
+void FunctionCorrector::visit(const ExprStmt *stmt) {
+}
+void FunctionCorrector::visit(const ForStmt *stmt) {
+}
+void FunctionCorrector::visit(const IfStmt *stmt) {
+}
+void FunctionCorrector::visit(const LoopStmt *stmt) {
+}
+void FunctionCorrector::visit(const ReturnStmt *stmt) {
+}
+void FunctionCorrector::visit(const SwitchCaseStmt *stmt) {
+}
+void FunctionCorrector::visit(const SwitchStmt *stmt) {
+}
+void FunctionCorrector::visit(const SwitchDefaultStmt *stmt) {
 }
 
 }// namespace ocarina
--- src/ast/function_corrector.h
+++ src/ast/function_corrector.h
@@ -12,21 +12,24 @@ class FunctionCorrector : public ExprVisitor, public StmtVisitor {
 private:
     Function *_function{};
 
+    /// key: old expression, value: new expression
+    std::map<const Expression *, const Expression *> _expr_map;
+
 public:
-    void visit(const AssignStmt *stmt) override {}
-    void visit(const BinaryExpr *stmt) override {}
+    void visit(const AssignStmt *stmt) override;
+    void visit(const BinaryExpr *stmt) override;
     void visit(const BreakStmt *stmt) override {}
     void visit(const CommentStmt *stmt) override {}
     void visit(const ContinueStmt *stmt) override {}
-    void visit(const ExprStmt *stmt) override {}
-    void visit(const ForStmt *stmt) override {}
-    void visit(const IfStmt *stmt) override {}
-    void visit(const LoopStmt *stmt) override {}
-    void visit(const ReturnStmt *stmt) override {}
+    void visit(const ExprStmt *stmt) override;
+    void visit(const ForStmt *stmt) override;
+    void visit(const IfStmt *stmt) override;
+    void visit(const LoopStmt *stmt) override;
+    void visit(const ReturnStmt *stmt) override;
     void visit(const ScopeStmt *stmt) override;
-    void visit(const SwitchCaseStmt *stmt) override {}
-    void visit(const SwitchStmt *stmt) override {}
-    void visit(const SwitchDefaultStmt *stmt) override {}
+    void visit(const SwitchCaseStmt *stmt) override;
+    void visit(const SwitchStmt *stmt) override;
+    void visit(const SwitchDefaultStmt *stmt) override;
     void visit(const PrintStmt *stmt) override {}
 
     void visit(const CallExpr *expr) override {}
