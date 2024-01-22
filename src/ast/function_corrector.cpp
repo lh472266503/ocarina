8fb12cf57428b640471db20cb5b18ef6117b8283
GraphZou <906520989@qq.com>
2024-01-22 23:11:25 +0800

    Update function_corrector.cpp

--- src/ast/function_corrector.cpp
+++ src/ast/function_corrector.cpp
@@ -37,7 +37,6 @@ void FunctionCorrector::traverse(Function &function) noexcept {
 
 void FunctionCorrector::apply(Function *function) noexcept {
     _function_stack.push_back(function);
-    OC_WARNING_FORMAT(" FunctionCorrector  {} ", function->description().c_str());
     traverse(*current_function());
     function->check_context();
     _function_stack.pop_back();
