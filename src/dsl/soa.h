c1bf2cfaedff4b0d9cd8b5d90559bb8faa528ee2
GraphZou <906520989@qq.com>
2024-02-25 18:00:39 +0800

    Update soa.h

--- src/dsl/soa.h
+++ src/dsl/soa.h
@@ -16,7 +16,24 @@ struct SOAView {
 
 template<>
 struct SOAView<uint> {
-    ByteBufferVar buffer;
+private:
+    Uint _offset{};
+    const ByteBufferVar *_buffer{};
+    uint _stride{};
+
+public:
+    SOAView(const Uint &ofs, const ByteBufferVar &buffer, uint stride)
+        : _offset(ofs), _buffer(&buffer), _stride(stride) {}
+
+    template<typename Index>
+    [[nodiscard]] Var<uint> read(Index &&index) noexcept {
+        return _buffer->load_as<uint>(_offset + OC_FORWARD(index) * sizeof(uint));
+    }
+
+    template<typename int_type = uint>
+    [[nodiscard]] Var<int_type> size_in_byte() noexcept {
+        return _buffer->size<int_type>() / _stride * uint(sizeof(uint));
+    }
 };
 
-}
+}// namespace ocarina
