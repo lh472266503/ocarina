55efff47c73593222290338d08833170e8819e56
GraphZou <906520989@qq.com>
2024-02-25 19:00:32 +0800

    Update soa.h

--- src/dsl/soa.h
+++ src/dsl/soa.h
@@ -14,26 +14,47 @@ struct SOAView {
     static_assert(always_false_v<T>);
 };
 
-template<>
-struct SOAView<uint> {
-private:
-    Uint _offset{};
-    const ByteBufferVar *_buffer{};
-    uint _stride{};
+#define OC_MAKE_SOA_ATOMIC_BODY(TypeName)                                          \
+    struct SOAView<TypeName> {                                                     \
+    public:                                                                        \
+        static constexpr uint size = sizeof(TypeName);                             \
+        using type = TypeName;                                                     \
+                                                                                   \
+    private:                                                                       \
+        Uint _offset{};                                                            \
+        const ByteBufferVar *_buffer{};                                            \
+        uint _stride{};                                                            \
+                                                                                   \
+    public:                                                                        \
+        SOAView(const Uint &ofs, const ByteBufferVar &buffer, uint stride)         \
+            : _offset(ofs), _buffer(&buffer), _stride(stride) {}                   \
+                                                                                   \
+        template<typename Index>                                                   \
+        [[nodiscard]] Var<float> read(Index &&index) noexcept {                    \
+            return _buffer->load_as<TypeName>(_offset + OC_FORWARD(index) * size); \
+        }                                                                          \
+                                                                                   \
+        template<typename int_type = uint>                                         \
+        [[nodiscard]] Var<int_type> size_in_byte() noexcept {                      \
+            return _buffer->size<int_type>() / _stride * size;                     \
+        }                                                                          \
+    };
 
-public:
-    SOAView(const Uint &ofs, const ByteBufferVar &buffer, uint stride)
-        : _offset(ofs), _buffer(&buffer), _stride(stride) {}
+#define OC_MAKE_SCALAR_SOA_ATOMIC(TypeName) \
+    template<>                              \
+    OC_MAKE_SOA_ATOMIC_BODY(TypeName)
 
-    template<typename Index>
-    [[nodiscard]] Var<uint> read(Index &&index) noexcept {
-        return _buffer->load_as<uint>(_offset + OC_FORWARD(index) * sizeof(uint));
-    }
+OC_MAKE_SCALAR_SOA_ATOMIC(uint)
+OC_MAKE_SCALAR_SOA_ATOMIC(uint64t)
+OC_MAKE_SCALAR_SOA_ATOMIC(float)
+OC_MAKE_SCALAR_SOA_ATOMIC(int)
 
-    template<typename int_type = uint>
-    [[nodiscard]] Var<int_type> size_in_byte() noexcept {
-        return _buffer->size<int_type>() / _stride * uint(sizeof(uint));
-    }
-};
+#define OC_COMMA ,
+
+template<typename T, uint N>
+OC_MAKE_SOA_ATOMIC_BODY(array<T OC_COMMA N>)
+
+#undef OC_MAKE_SCALAR_SOA_ATOMIC
+#undef OC_MAKE_SOA_ATOMIC_BODY
 
 }// namespace ocarina
