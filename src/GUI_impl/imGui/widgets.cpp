40b7115285eee57ad81edb74e51e27aa755e3ec2
GraphZou <906520989@qq.com>
2024-03-17 15:34:34 +0800

    Update widgets.cpp

--- src/GUI_impl/imGui/widgets.cpp
+++ src/GUI_impl/imGui/widgets.cpp
@@ -7,6 +7,7 @@
 namespace ocarina {
 
 void ImGuiWidgets::init() noexcept {
+    ImGui::CreateContext();
 }
 void ImGuiWidgets::push_window(const char *label) noexcept {
     ImGui::Begin(label);
@@ -30,22 +31,22 @@ bool ImGuiWidgets::slider_float2(const char *label, float2 *val, float min, floa
     return ImGui::SliderFloat2(label, reinterpret_cast<float *>(val), min, max);
 }
 bool ImGuiWidgets::slider_float3(const char *label, float3 *val, float min, float max) noexcept {
-    return false;
+    return ImGui::SliderFloat3(label, reinterpret_cast<float *>(val), min, max);
 }
 bool ImGuiWidgets::slider_float4(const char *label, float4 *val, float min, float max) noexcept {
-    return false;
+    return ImGui::SliderFloat4(label, reinterpret_cast<float *>(val), min, max);
 }
 bool ImGuiWidgets::slider_int(const char *label, int *val, int min, int max) noexcept {
-    return false;
+    return ImGui::SliderInt(label, val, min, max);
 }
 bool ImGuiWidgets::slider_int2(const char *label, int2 *val, int min, int max) noexcept {
-    return false;
+    return ImGui::SliderInt2(label, reinterpret_cast<int *>(val), min, max);
 }
 bool ImGuiWidgets::slider_int3(const char *label, int3 *val, int min, int max) noexcept {
-    return false;
+    return ImGui::SliderInt3(label, reinterpret_cast<int *>(val), min, max);
 }
 bool ImGuiWidgets::slider_int4(const char *label, int4 *val, int min, int max) noexcept {
-    return false;
+    return ImGui::SliderInt4(label, reinterpret_cast<int *>(val), min, max);
 }
 bool ImGuiWidgets::color_edit(const char *label, float3 *val) noexcept {
     return false;
