e4c9af71616352023085f5e10255f80b8bf36345
GraphZou <906520989@qq.com>
2024-03-23 00:53:24 +0800

    Update gl_interop.h

--- src/backends/cuda/gl_interop.h
+++ src/backends/cuda/gl_interop.h
@@ -8,6 +8,7 @@
 #include <cuda_gl_interop.h>
 #include "rhi/resources/gl_interop.h"
 #include "rhi/resources/texture.h"
+#include "util.h"
 
 namespace ocarina {
 
@@ -23,11 +24,13 @@ public:
 
         uint2 res = _texture->resolution().xy();
         uint stride = _texture->pixel_size();
-
         CHECK_GL(glBufferData(GL_ARRAY_BUFFER, stride * res.x * res.y,
                               nullptr, GL_STREAM_DRAW));
-
         CHECK_GL(glBindBuffer(GL_ARRAY_BUFFER, 0u));
+        OC_CUDA_CHECK(cudaGraphicsGLRegisterBuffer(
+            &_cuda_graphics_resource,
+            _pbo,
+            cudaGraphicsMapFlagsWriteDiscard));
     }
     void mapping() noexcept override {
     }
