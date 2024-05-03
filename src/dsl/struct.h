3cacd5e3ecc16173227e6e3f68ed05ba263cf1f7
GraphZou <906520989@qq.com>
2024-05-03 23:08:16 +0800

    Update struct.h

--- src/dsl/struct.h
+++ src/dsl/struct.h
@@ -8,7 +8,12 @@
 #include "var.h"
 #include "soa.h"
 
-#define OC_STRUCT_IMPL(NS, S, ...)                                        \
+#define OC_STRUCT_ALIAS(NS, S)      \
+    namespace NS {                  \
+    using S##Var = ocarina::Var<S>; \
+    }
+
+#define OC_STRUCT_IMPL(NS, S, ...)                                            \
     OC_MAKE_STRUCT_REFLECTION(NS::S, ##__VA_ARGS__)                           \
     OC_MAKE_STRUCT_DESC(NS::S, ##__VA_ARGS__)                                 \
     OC_MAKE_COMPUTABLE_BODY(NS::S, ##__VA_ARGS__)                             \
@@ -18,11 +23,11 @@
 #define OC_STRUCT(NS, S, ...) \
     OC_STRUCT_IMPL(NS, S, ##__VA_ARGS__)
 
-#define OC_BUILTIN_STRUCT(NS,S, ...) \
+#define OC_BUILTIN_STRUCT(NS, S, ...) \
     OC_MAKE_BUILTIN_STRUCT(NS::S)     \
-    OC_STRUCT_IMPL(NS,S, ##__VA_ARGS__)
+    OC_STRUCT_IMPL(NS, S, ##__VA_ARGS__)
 
-#define OC_PARAM_STRUCT(NS,S, ...)                 \
+#define OC_PARAM_STRUCT(NS, S, ...)                 \
     OC_MAKE_PARAM_STRUCT(NS::S)                     \
     OC_MAKE_STRUCT_REFLECTION(NS::S, ##__VA_ARGS__) \
     OC_MAKE_STRUCT_DESC(NS::S, ##__VA_ARGS__)       \
