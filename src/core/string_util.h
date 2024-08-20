40dd791f5ff032a9a6aa4baf0adeb403be1c424b
GraphZou <906520989@qq.com>
2024-08-20 14:46:58 +0800

    Update string_util.h

--- src/core/string_util.h
+++ src/core/string_util.h
@@ -195,13 +195,19 @@ template<typename FMT, typename... Args>
     return ret;
 }
 
-[[nodiscard]] inline std::string replace(std::string str, const std::string& from, const std::string& to) {
+[[nodiscard]] inline std::string string_replace(std::string str, const std::string_view &from,
+                                                const std::string_view &to) {
     size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length();
     }
     return str;
+}
+
+[[nodiscard]] inline bool string_contain(const std::string_view &string,
+                                         const std::string_view &sub) {
+    return string.find(sub) != std::string::npos;
 }
 
 [[nodiscard]] inline std::string from_file(const fs::path &fn) {
