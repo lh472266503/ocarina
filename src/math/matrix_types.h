fc7e17ef1d81db8bc76de1ecdc1ecbc1e468b432
GraphZou <906520989@qq.com>
2024-10-08 11:52:50 +0800

    Update matrix_types.h

--- src/math/matrix_types.h
+++ src/math/matrix_types.h
@@ -57,6 +57,48 @@ public:
     [[nodiscard]] constexpr const vector_type &operator[](size_t i) const noexcept { return cols_[i]; }
 };
 
+#define OC_MATRIX_UNARY_FUNC(func)                             \
+    template<size_t N, size_t M>                               \
+    [[nodiscard]] Matrix<N, M> func(Matrix<N, M> m) noexcept { \
+        return [&]<size_t... i>(std::index_sequence<i...>) {   \
+            return ocarina::Matrix<N, M>(func(m[i])...);       \
+        }(std::make_index_sequence<N>());                      \
+    }
+
+OC_MATRIX_UNARY_FUNC(rcp)
+OC_MATRIX_UNARY_FUNC(abs)
+OC_MATRIX_UNARY_FUNC(sqrt)
+OC_MATRIX_UNARY_FUNC(sqr)
+OC_MATRIX_UNARY_FUNC(sign)
+OC_MATRIX_UNARY_FUNC(cos)
+OC_MATRIX_UNARY_FUNC(sin)
+OC_MATRIX_UNARY_FUNC(tan)
+OC_MATRIX_UNARY_FUNC(cosh)
+OC_MATRIX_UNARY_FUNC(sinh)
+OC_MATRIX_UNARY_FUNC(tanh)
+OC_MATRIX_UNARY_FUNC(log)
+OC_MATRIX_UNARY_FUNC(log2)
+OC_MATRIX_UNARY_FUNC(log10)
+OC_MATRIX_UNARY_FUNC(exp)
+OC_MATRIX_UNARY_FUNC(exp2)
+OC_MATRIX_UNARY_FUNC(asin)
+OC_MATRIX_UNARY_FUNC(acos)
+OC_MATRIX_UNARY_FUNC(atan)
+OC_MATRIX_UNARY_FUNC(asinh)
+OC_MATRIX_UNARY_FUNC(acosh)
+OC_MATRIX_UNARY_FUNC(atanh)
+OC_MATRIX_UNARY_FUNC(floor)
+OC_MATRIX_UNARY_FUNC(ceil)
+OC_MATRIX_UNARY_FUNC(degrees)
+OC_MATRIX_UNARY_FUNC(radians)
+OC_MATRIX_UNARY_FUNC(round)
+OC_MATRIX_UNARY_FUNC(isnan)
+OC_MATRIX_UNARY_FUNC(isinf)
+OC_MATRIX_UNARY_FUNC(fract)
+OC_MATRIX_UNARY_FUNC(copysign)
+
+#undef OC_MATRIX_UNARY_FUNC
+
 }// namespace ocarina
 
 template<size_t N, size_t M>
