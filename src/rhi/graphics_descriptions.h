7b35f5a02c00b8d76ff11c1bd503433ac822fd84
Liang Ai Ran <ai-ran.liang@ubisoft.com>
2024-10-15 13:58:42 +0800

    vulkan backend update

---
+++ src/rhi/graphics_descriptions.h
@@ -0,0 +1,43 @@
+//
+// Created by Zero on 2022/8/10.
+//
+
+#pragma once
+
+#include "core/stl.h"
+#include "core/image_base.h"
+
+namespace ocarina {
+
+struct InstanceCreation {
+    const char *applicationName;
+    bool validation = false;
+    std::vector<const char *> instanceExtentions;
+};
+
+struct SwapChainCreation {
+    PixelStorage format = PixelStorage::BYTE4; ///< Back buffers format.
+    ColorSpace colorSpace = ColorSpace::SRGB;   ///< Back buffers color space.
+    uint32_t bufferCount = 1;         ///< Back buffers count.
+    uint32_t width = 800;          ///< Back buffers width.
+    uint32_t height = 600;              ///< Back buffers height.
+    uint32_t refreshRate = 60;    ///< Refresh rate in Hz.
+    bool vsync = false;///< 0 - The presentation occurs immediately, there is no synchronization. 1 through 4 - Synchronize presentation after the nth vertical blank.
+    bool isWindowed = true;   //.
+};
+
+enum QueueType
+{
+    Graphics,///< Graphics queue
+    Compute, ///< Compute queue
+    Copy,    ///< Copy queue
+    NumQueueType,
+};
+
+//enum CommandListType {
+//    Graphics,///< Graphics command list
+//    Compute, ///< Compute command list
+//    Copy     ///< Copy command list
+//};
+
+}// namespace ocarina
