d63713dded62a5924d700fa4ff2e671213e3a761
GraphZou <906520989@qq.com>
2024-10-29 12:33:33 +0800

    update py

--- src/python/export_vector.h
+++ src/python/export_vector.h
@@ -13,22 +13,22 @@
 namespace py = pybind11;
 using namespace ocarina;
 
-#define OC_EXPORT_ARITHMETIC_OP(type, dim)                                                                                     \
-    m = m.def("__add__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a + b; }, py::is_operator());     \
-    m = m.def("__add__", [](const Vector<type, dim> &a, const type &b) { return a + b; }, py::is_operator());                  \
-    m = m.def("__radd__", [](const Vector<type, dim> &a, const type &b) { return a + b; }, py::is_operator());                 \
-    m = m.def("__sub__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a - b; }, py::is_operator());     \
-    m = m.def("__sub__", [](const Vector<type, dim> &a, const type &b) { return a - b; }, py::is_operator());                  \
-    m = m.def("__rsub__", [](const Vector<type, dim> &a, const type &b) { return b - a; }, py::is_operator());                 \
-    m = m.def("__mul__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a * b; }, py::is_operator());     \
-    m = m.def("__mul__", [](const Vector<type, dim> &a, const type &b) { return a * b; }, py::is_operator());                  \
-    m = m.def("__rmul__", [](const Vector<type, dim> &a, const type &b) { return a * b; }, py::is_operator());                 \
-    m = m.def("__truediv__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a / b; }, py::is_operator()); \
-    m = m.def("__truediv__", [](const Vector<type, dim> &a, const type &b) { return a / b; }, py::is_operator());              \
-    m = m.def("__rtruediv__", [](const Vector<type, dim> &a, const type &b) { return b / a; }, py::is_operator());             \
-    m = m.def("__gt__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a > b; }, py::is_operator());      \
-    m = m.def("__ge__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a >= b; }, py::is_operator());     \
-    m = m.def("__lt__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a < b; }, py::is_operator());      \
-    m = m.def("__le__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a <= b; }, py::is_operator());     \
-    m = m.def("__eq__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a == b; }, py::is_operator());     \
-    m = m.def("__ne__", [](const Vector<type, dim> &a, const Vector<type, dim> &b) { return a != b; }, py::is_operator());\
+#define OC_EXPORT_ARITHMETIC_OP(type, dim)                                                                               \
+    m = m.def("__add__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a + b; }, py::is_operator());     \
+    m = m.def("__add__", [](const Vector<T, Dim> &a, const T &Dim) { return a + b; }, py::is_operator());                \
+    m = m.def("__radd__", [](const Vector<T, Dim> &a, const T &Dim) { return a + b; }, py::is_operator());               \
+    m = m.def("__sub__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a - b; }, py::is_operator());     \
+    m = m.def("__sub__", [](const Vector<T, Dim> &a, const T &Dim) { return a - b; }, py::is_operator());                \
+    m = m.def("__rsub__", [](const Vector<T, Dim> &a, const T &Dim) { return b - a; }, py::is_operator());               \
+    m = m.def("__mul__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a * b; }, py::is_operator());     \
+    m = m.def("__mul__", [](const Vector<T, Dim> &a, const T &Dim) { return a * b; }, py::is_operator());                \
+    m = m.def("__rmul__", [](const Vector<T, Dim> &a, const T &Dim) { return a * b; }, py::is_operator());               \
+    m = m.def("__truediv__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a / b; }, py::is_operator()); \
+    m = m.def("__truediv__", [](const Vector<T, Dim> &a, const T &Dim) { return a / b; }, py::is_operator());            \
+    m = m.def("__rtruediv__", [](const Vector<T, Dim> &a, const T &Dim) { return b / a; }, py::is_operator());           \
+    m = m.def("__gt__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a > b; }, py::is_operator());      \
+    m = m.def("__ge__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a >= b; }, py::is_operator());     \
+    m = m.def("__lt__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a < b; }, py::is_operator());      \
+    m = m.def("__le__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a <= b; }, py::is_operator());     \
+    m = m.def("__eq__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a == b; }, py::is_operator());     \
+    m = m.def("__ne__", [](const Vector<T, Dim> &a, const Vector<T, Dim> &b) { return a != b; }, py::is_operator());\
--- src/python/export_vector2.cpp
+++ src/python/export_vector2.cpp
@@ -7,14 +7,55 @@
 namespace py = pybind11;
 using namespace ocarina;
 
-#define OC_EXPORT_VECTOR2(type)                                                                       \
-    py::class_<ocarina::detail::VectorStorage<type, 2>>(m, "_VectorStorage" #type "2");               \
-    auto m##type = py::class_<Vector<type, 2>, ocarina::detail::VectorStorage<type, 2>>(m, #type "2") \
-                       .def(py::init<>())                                                             \
-                       .def(py::init<type>())                                                         \
-                       .def(py::init<type, type>())                                                   \
-                       .def("__repr__", [](Vector<type, 2> &self) { return format(#type "2({},{})", self.x, self.y); })
+#define OC_EXPORT_VECTOR2(T)                                                                                   \
+    py::class_<ocarina::detail::VectorStorage<T, 2>>(m, "_VectorStorage" #T "2");                              \
+    auto m##T = py::class_<Vector<T, 2>, ocarina::detail::VectorStorage<T, 2>>(m, #T "2")                      \
+                    .def(py::init<>())                                                                         \
+                    .def(py::init<T>())                                                                        \
+                    .def(py::init<T, T>())                                                                     \
+                    .def("__repr__", [](Vector<T, 2> &self) { return format(#T "2({},{})", self.x, self.y); }) \
+                    .def("__getitem__", [](Vector<T, 2> &self, size_t i) { return self[i]; })                  \
+                    .def("__setitem__", [](Vector<T, 2> &self, size_t i, T k) { self[i] = k; })                \
+                    .def("copy", [](Vector<T, 2> &self) { return Vector<T, 2>(self); })                        \
+                    .def_readwrite("x", &Vector<T, 2>::x)                                                      \
+                    .def_readwrite("y", &Vector<T, 2>::y)                                                      \
+                    .def_readwrite("xx", [](Vector<T, 2> &self) { return self.xx().decay(); })   \
+                    .def_readwrite("xy", [](Vector<T, 2> &self) { return self.xy().decay(); })
 
 void export_vector2(py::module &m) {
-    OC_EXPORT_VECTOR2(float);
+#define OC_EXPORT_VECTOR2(T)                                                                                   \
+    py::class_<ocarina::detail::VectorStorage<T, 2>>(m, "_VectorStorage" #T "2");                              \
+    auto m##T = py::class_<Vector<T, 2>, ocarina::detail::VectorStorage<T, 2>>(m, #T "2")                      \
+                    .def(py::init<>())                                                                         \
+                    .def(py::init<T>())                                                                        \
+                    .def(py::init<T, T>())                                                                     \
+                    .def("__repr__", [](Vector<T, 2> &self) { return format(#T "2({},{})", self.x, self.y); }) \
+                    .def("__getitem__", [](Vector<T, 2> &self, size_t i) { return self[i]; })                  \
+                    .def("__setitem__", [](Vector<T, 2> &self, size_t i, T k) { self[i] = k; })                \
+                    .def("copy", [](Vector<T, 2> &self) { return Vector<T, 2>(self); })                        \
+                    .def_readwrite("x", &Vector<T, 2>::x)                                                      \
+                    .def_readwrite("y", &Vector<T, 2>::y)                                                      \
+                    .def_readwrite("xx", [](Vector<T, 2> &self) { return self.xx().decay(); })   \
+                    .def_readwrite("xy", [](Vector<T, 2> &self) { return self.xy().decay(); })
+Replacement:
+    py::class_<ocarina::detail::VectorStorage<float, 2>>(m, "_VectorStorage"
+                                                            "float"
+                                                            "2");
+    auto mfloat = py::class_<Vector<float, 2>, ocarina::detail::VectorStorage<float, 2>>(m, "float"
+                                                                                            "2")
+                      .def(py::init<>())
+                      .def(py::init<float>())
+                      .def(py::init<float, float>())
+                      .def("__repr__", [](Vector<float, 2> &self) { return format("float"
+                                                                                  "2({},{})",
+                                                                                  self.x, self.y); })
+                      .def("__getitem__", [](Vector<float, 2> &self, size_t i) { return self[i]; })
+                      .def("__setitem__", [](Vector<float, 2> &self, size_t i, float k) { self[i] = k; })
+                      .def("copy", [](Vector<float, 2> &self) { return Vector<float, 2>(self); })
+                      .def_readwrite("x", &Vector<float, 2>::x)
+                      .def_readwrite("y", &Vector<float, 2>::y)
+                      .def_property("xx", [](Vector<float, 2> &self) { return self.xx().decay(); }, [](Vector<float, 2> &self, Vector<float, 2> v) {
+                              self.xx() = v;
+                          });
+//    OC_EXPORT_VECTOR2(float);
 }
