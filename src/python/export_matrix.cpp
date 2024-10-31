ea23471ffc735b8a7265319ffadbe34df07fba72
GraphZou <906520989@qq.com>
2024-10-31 11:42:20 +0800

    Update export_matrix.cpp

--- src/python/export_matrix.cpp
+++ src/python/export_matrix.cpp
@@ -9,7 +9,49 @@ namespace py = pybind11;
 using namespace ocarina;
 
 template<size_t N, size_t M, typename Module>
-void export_matrix_op(py::module &m, Module &mt) {
+void export_matrix_func(py::module &m, Module &mt) {
+#define OC_EXPORT_MATRIX_FUNC(func) \
+    m.def(#func, [&](Matrix<N, M> mat) { return ocarina::func(mat); });
+
+    if constexpr (N == M) {
+        OC_EXPORT_MATRIX_FUNC(inverse)
+        OC_EXPORT_MATRIX_FUNC(transpose)
+        OC_EXPORT_MATRIX_FUNC(determinant)
+    }
+
+    OC_EXPORT_MATRIX_FUNC(rcp)
+    OC_EXPORT_MATRIX_FUNC(abs)
+    OC_EXPORT_MATRIX_FUNC(sqrt)
+    OC_EXPORT_MATRIX_FUNC(sqr)
+    OC_EXPORT_MATRIX_FUNC(sign)
+    OC_EXPORT_MATRIX_FUNC(cos)
+    OC_EXPORT_MATRIX_FUNC(sin)
+    OC_EXPORT_MATRIX_FUNC(tan)
+    OC_EXPORT_MATRIX_FUNC(cosh)
+    OC_EXPORT_MATRIX_FUNC(sinh)
+    OC_EXPORT_MATRIX_FUNC(tanh)
+    OC_EXPORT_MATRIX_FUNC(log)
+    OC_EXPORT_MATRIX_FUNC(log2)
+    OC_EXPORT_MATRIX_FUNC(log10)
+    OC_EXPORT_MATRIX_FUNC(exp)
+    OC_EXPORT_MATRIX_FUNC(exp2)
+    OC_EXPORT_MATRIX_FUNC(asin)
+    OC_EXPORT_MATRIX_FUNC(acos)
+    OC_EXPORT_MATRIX_FUNC(atan)
+    OC_EXPORT_MATRIX_FUNC(asinh)
+    OC_EXPORT_MATRIX_FUNC(acosh)
+    OC_EXPORT_MATRIX_FUNC(atanh)
+    OC_EXPORT_MATRIX_FUNC(floor)
+    OC_EXPORT_MATRIX_FUNC(ceil)
+    OC_EXPORT_MATRIX_FUNC(degrees)
+    OC_EXPORT_MATRIX_FUNC(radians)
+    OC_EXPORT_MATRIX_FUNC(round)
+    OC_EXPORT_MATRIX_FUNC(isnan)
+    OC_EXPORT_MATRIX_FUNC(isinf)
+    OC_EXPORT_MATRIX_FUNC(fract)
+
+#undef OC_EXPORT_MATRIX_FUNC
+
 }
 
 template<size_t N, size_t M>
@@ -23,6 +65,9 @@ auto export_matrix_base(py::module &m) {
     mt.def("__rmul__", [](Matrix<N, M> &self, float s) { return self * s; });
     mt.def("__mul__", [](Matrix<N, M> &self, ocarina::Vector<float, N> v) { return self * v; });
     mt.def("__mul__", [](Matrix<N, M> &self, ocarina::Matrix<M, N> rhs) { return self * rhs; });
+    mt.def("__truediv__", [](Matrix<N, M> &self,  float s) { return self / s; });
+    mt.def("__add__", [](Matrix<N, M> &self, ocarina::Matrix<N, M> rhs) { return self + rhs; });
+    mt.def("__sub__", [](Matrix<N, M> &self, ocarina::Matrix<N, M> rhs) { return self - rhs; });
 
     auto make_func_name = "make_" + cls_name;
     auto export_constructor = [&]<typename Func>(const Func &func) {
@@ -53,15 +98,6 @@ auto export_matrix_base(py::module &m) {
     mt.def("__repr__", [](Matrix<N, M> &self) {
         return to_str(self);
     });
-
-    if constexpr (N == M) {
-#define OC_EXPORT_MATRIX_FUNC(func) \
-    m.def(#func, [&](Matrix<N, M> mat) { return ocarina::func(mat); });
-        OC_EXPORT_MATRIX_FUNC(inverse)
-        OC_EXPORT_MATRIX_FUNC(transpose)
-        OC_EXPORT_MATRIX_FUNC(determinant)
-#undef OC_EXPORT_MATRIX_FUNC
-    }
     
     return mt;
 }
@@ -69,7 +105,7 @@ auto export_matrix_base(py::module &m) {
 template<size_t N, size_t M>
 void export_matrix_(py::module &m) {
     auto mt = export_matrix_base<N, M>(m);
-    export_matrix_op<N, M>(m, mt);
+    export_matrix_func<N, M>(m, mt);
 }
 
 void export_matrix(py::module &m) {
