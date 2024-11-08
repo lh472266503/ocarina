39fc680d6a11d3360d996a19c4adaf70d4e5b00e
GraphZou <906520989@qq.com>
2024-11-08 21:54:26 +0800

    ,

--- src/python/ast/export_ast.cpp
+++ src/python/ast/export_ast.cpp
@@ -9,7 +9,9 @@ namespace py = pybind11;
 using namespace ocarina;
 
 void export_type(PythonExporter &exporter);
+void export_expressions(PythonExporter &exporter);
 
 void export_ast(PythonExporter &exporter) {
     export_type(exporter);
+    export_expressions(exporter);
 }
---
+++ src/python/ast/export_expressions.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Zero on 2024/11/8.
+//
+
+#include "python/ocapi.h"
+#include "math/base.h"
+
+namespace py = pybind11;
+using namespace ocarina;
+
+void export_expressions(PythonExporter &exporter) {
+
+}
