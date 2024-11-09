2602f9755af6b2259416a0a2b74c0ad61a02f8fa
GraphZou <906520989@qq.com>
2024-11-09 22:20:56 +0800

    update function

--- src/pyexporter/ast/export_ast.cpp
+++ src/pyexporter/ast/export_ast.cpp
@@ -12,9 +12,20 @@ void export_type(PythonExporter &exporter);
 void export_expressions(PythonExporter &exporter);
 
 void export_function(PythonExporter &exporter) {
-    auto mt = py::class_<Function, Hashable, concepts::Noncopyable>(exporter.module, "Function");
-    mt.def_static("push", []() {
+    using FunctionTag = Function::Tag;
+    OC_EXPORT_ENUM(exporter.module, FunctionTag, KERNEL, CALLABLE)
 
+    auto mt = py::class_<Function, Hashable, concepts::Noncopyable>(exporter.module, "Function");
+    mt.def_static("push", [](FunctionTag tag) {
+        auto ret = make_shared<Function>(tag);
+        Function::push(ret);
+        return ret;
+    });
+
+    mt.def_static("pop", [] {
+        auto ret = Function::current();
+        Function::pop(nullptr);
+        return ret;
     });
 }
 
