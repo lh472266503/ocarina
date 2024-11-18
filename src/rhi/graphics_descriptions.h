7f0f3ef0ca8187668354198f94cbd5140c665761
Liang Ai Ran <ai-ran.liang@ubisoft.com>
2024-11-18 10:04:11 +0800

    adjust target folders

--- src/rhi/graphics_descriptions.h
+++ src/rhi/graphics_descriptions.h
@@ -27,12 +27,38 @@ struct SwapChainCreation {
     bool isWindowed = true;   //.
 };
 
+enum class GraphicBufferBindFlags {
+    None = (0 << 0),                    ///< None
+    VertexBuffer = (1 << 0),            ///< Can bind as a vertex buffer
+    IndexBuffer = (1 << 1),             ///< Can bind as an index buffer
+    ConstantBuffer = (1 << 2),          ///< Can bind as a constant buffer.
+    StructuredBuffer = (1 << 3),        ///< Can bind as a structured buffer.
+    ByteAddressBuffer = (1 << 4),       ///< Can bind as a byte address buffer.
+    FormattedBuffer = (1 << 5),         ///< Can bind as a formatted buffer.
+    CopySrc = (1 << 6),                 ///< Can bind as a copy source.
+    CopyDst = (1 << 7),                 ///< Can bind as a copy destination.
+    ShaderReadWrite = (1 << 8),         ///< Can bind for usage as ReadWrite buffer for any shader stage (UAV on D3D12).
+    PredicationExt = (1 << 9),          ///< Can use the buffer for SetPredicationEx calls (must have AdapterFeatures::Predication requested feature enabled)
+    AccelerationStructureExt = (1 << 10)///< Can bind as a ray tracing acceleration structure (require AdapterFeatures::RayTracing)
+};
+
 enum QueueType
 {
     Graphics,///< Graphics queue
     Compute, ///< Compute queue
     Copy,    ///< Copy queue
     NumQueueType,
+};
+
+struct BufferCreation {
+    AccessFlags AccessFlags;   ///< Access flags.
+    GraphhicBufferBindFlags BindFlags; ///< Buffer bind flags.
+    BufferFlags Flags;         ///< Buffer flags.
+    uint32_t ElementSize;           ///< Element size in bytes.
+    U32 NumElements;           ///< Number of elements in the resource.
+    Format Format;             ///< Data format.
+    ResourceState InitialState;///< Initial resource state.
+    Extension Extensions;      ///< Extensions
 };
 
 //enum CommandListType {
--- src/tests/test_vulkan_triangle.cpp
+++ src/tests/test_vulkan_triangle.cpp
@@ -23,13 +23,13 @@ int main(int argc, char *argv[]) {
     auto window = file_manager.create_window("display", make_uint2(800, 600), WindowLibrary::SDL3);
 
     InstanceCreation instanceCreation = {};
-    //instanceCreation.instanceExtentions = 
+    //instanceCreation.instanceExtentions =
     instanceCreation.windowHandle = window->get_window_handle();
     Device device = Device::create_device("vulkan", instanceCreation);
     Stream stream = device.create_stream();
 
-    auto image_io = Image::pure_color(make_float4(1,0,0,1), ColorSpace::LINEAR, make_uint2(500));
-    window->run([&](double d){
+    auto image_io = Image::pure_color(make_float4(1, 0, 0, 1), ColorSpace::LINEAR, make_uint2(500));
+    window->run([&](double d) {
         window->set_background(image_io.pixel_ptr<float4>(), make_uint2(800, 600));
     });
 }
