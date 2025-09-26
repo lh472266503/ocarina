808bf986f4249298ccd64f75c1e82973957f2fc9
GraphZou <906520989@qq.com>
2025-09-26 15:12:26 +0800

    update stl

--- src/core/stl.h
+++ src/core/stl.h
@@ -361,42 +361,11 @@ using std::variant;
 using std::visit;
 namespace fs = std::filesystem;
 
-[[nodiscard]] inline fs::path parent_path(const fs::path &p,
-                                          int levels) {
-    fs::path cur_path = p;
-    for (int i = 0; i < levels; ++i) {
-        cur_path = cur_path.parent_path();
-    }
-    return cur_path;
-}
+[[nodiscard]] fs::path parent_path(const fs::path &p, int levels);
 
-inline void clear_directory(const std::filesystem::path &dir_path) {
-    try {
-        if (std::filesystem::exists(dir_path) && std::filesystem::is_directory(dir_path)) {
-            for (const auto &entry : std::filesystem::directory_iterator(dir_path)) {
-                std::filesystem::remove_all(entry.path());
-            }
-            std::cout << "Directory cleared: " << dir_path << std::endl;
-        } else {
-            std::cout << "Directory does not exist: " << dir_path << std::endl;
-        }
-    } catch (const std::filesystem::filesystem_error &e) {
-        std::cerr << "Error clearing directory: " << e.what() << std::endl;
-    }
-}
+void clear_directory(const std::filesystem::path &dir_path);
 
-inline std::string get_file_name(const std::string& file_path)
-{
-    auto it = std::find_if(file_path.rbegin(), file_path.rend(), [](const char c) {
-        return c == '\\' || c == '/';
-    });
-    if (it == file_path.rend())
-    {
-        return file_path;
-    }
-
-    return file_path.substr(it.base() - file_path.begin());
-}
+std::string get_file_name(const std::string& file_path);
 
 inline std::string get_file_directory(const std::string& file_path)
 {
